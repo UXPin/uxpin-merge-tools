ddfe8d6b8e882cc58b71fc1ca77c178ebcb42457
Michał Stocki <michal.stocki@uxpin.com>
2017-09-07 15:19:35 +0200

    10272: Added test for `--dump` option

--- packages/uxpin-code-cli/test/integration/dump/dumpOption.test.ts
+++ packages/uxpin-code-cli/test/integration/dump/dumpOption.test.ts
@@ -4,10 +4,24 @@ describe('--dump option integration', () => {
   describe('--dump option prints ', () => {
     it('', () => {
       // when
-      runUxPinCodeCommand('--dump').then((output) => {
+      return runUxPinCodeCommand('resources/repos/arui-feather', '--dump').then((output) => {
         // then
-        expect(output).toContain('--dump');
-        expect(output).toContain('Show all information about the design system repository and NOT send to UXPin')
+        expect(output).toContain(`
+amount
+app-content
+app-menu
+app-title
+attach
+button
+calendar
+calendar-input
+card-input
+checkbox-group
+checkbox
+collapse
+copyright
+dropdown
+email-input`);
       });
     });
   });
--- packages/uxpin-code-cli/test/utils/runUxPinCodeCommand.ts
+++ packages/uxpin-code-cli/test/utils/runUxPinCodeCommand.ts
@@ -5,8 +5,13 @@ import { runCommand } from './runCommand';
 const nycPath:string = path.join(__dirname, '../../node_modules/.bin/nyc');
 const uxPinPath:string = path.join(__dirname, '../../bin/uxpin-code');
 
-export function runUxPinCodeCommand(options?:string):Promise<string> {
+export function runUxPinCodeCommand(workingDir:string, options?:string):Promise<string> {
   const coverageDirName:string = getRandomString();
+  const absoluteWorkingDir:string = getAbsoluteWorkingDir(workingDir);
   const coverageCommand:string = `${nycPath} --reporter=lcov --report-dir=./coverage-cli/${coverageDirName}`;
-  return runCommand(`${coverageCommand} ${uxPinPath} ${options}`);
+  return runCommand(`cd ${absoluteWorkingDir} ${coverageCommand} ${uxPinPath} ${options}`);
+}
+
+function getAbsoluteWorkingDir(pathRelativeToTestDir:string) {
+  return path.join(__dirname, '../', pathRelativeToTestDir);
 }
