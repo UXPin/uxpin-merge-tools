ed6363a3b92b0ec9cf394a39ebbbf12529258eed
Michał Stocki <michal.stocki@uxpin.com>
2017-09-07 14:18:43 +0200

    10272: Added test to `--help` option

---
+++ packages/uxpin-code-cli/test/integration/help/helpOption.test.ts
@@ -0,0 +1,12 @@
+import { runUxPinCodeCommand } from '../../utils/runUxPinCodeCommand';
+
+describe('--help option', () => {
+  it('it prints help for --dump option', () => {
+    // when
+    runUxPinCodeCommand('--help').then((output) => {
+      // then
+      expect(output).toContain('--dump');
+      expect(output).toContain('Show all information about the design system repository and NOT send to UXPin')
+    });
+  });
+});
