46d0565feff1ee7c0687485799939645beca1d7a
Michał Stocki <michal.stocki@uxpin.com>
2017-09-13 23:04:53 +0200

    10272: Fixed recognizing TypeScript components, removed strategy provider

--- packages/uxpin-code-cli/src/serialization/props/serializeComponentProps.ts
+++ packages/uxpin-code-cli/src/serialization/props/serializeComponentProps.ts
@@ -1,6 +1,13 @@
+import { extname } from 'path';
 import { ComponentPropsList } from './ComponentPropsList';
-import { getComponentSerializationStrategy } from './getComponentSerializationStrategy';
+import { serializeJSComponentProps } from './javascript/serializeJSComponentProps';
+import { serializeTSComponentProps } from './typescript/serializeTSComponentProps';
 
 export function serializeComponentProps(componentFileLocation:string):Promise<ComponentPropsList> {
-  return getComponentSerializationStrategy(componentFileLocation)(componentFileLocation);
+  const TYPESCRIPT_COMPONENT_EXTENSION:string = '.tsx';
+  const extension:string = extname(componentFileLocation);
+  if (extension === TYPESCRIPT_COMPONENT_EXTENSION) {
+    return serializeTSComponentProps(componentFileLocation);
+  }
+  return serializeJSComponentProps(componentFileLocation);
 }
