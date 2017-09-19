a93d6825d6b5cccf3ca38586c2d161a46a5d5ebf
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2017-09-19 09:31:35 +0200

    10272: CR fix Fix typo in word UXPin

--- packages/uxpin-code-cli/test/integration/help/helpOption.test.ts
+++ packages/uxpin-code-cli/test/integration/help/helpOption.test.ts
@@ -1,9 +1,9 @@
-import { runUxPinCodeCommand } from '../../utils/runUxPinCodeCommand';
+import { runUXPinCodeCommand } from '../../utils/runUXPinCodeCommand';
 
 describe('--help option', () => {
   it('it prints help for --dump option', () => {
     // when
-    return runUxPinCodeCommand('./', '--help').then((output) => {
+    return runUXPinCodeCommand('./', '--help').then((output) => {
       // then
       expect(output).toContain('--dump');
       expect(output).toContain('Show all information about the design system repository and NOT send to UXPin');
@@ -12,7 +12,7 @@ describe('--help option', () => {
 
   it('it prints help for --summary option', () => {
     // when
-    return runUxPinCodeCommand('./', '--help').then((output) => {
+    return runUXPinCodeCommand('./', '--help').then((output) => {
       // then
       expect(output).toContain('--summary');
       expect(output).toContain('Show design system summary');
