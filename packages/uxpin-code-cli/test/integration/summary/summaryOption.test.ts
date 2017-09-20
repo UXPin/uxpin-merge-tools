e9deafd08d3f17ff9a31c5f299e47dc0190ccf49
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2017-09-20 12:09:34 +0200

    10513: Add summary test for polaris

--- packages/uxpin-code-cli/test/integration/summary/summaryOption.test.ts
+++ packages/uxpin-code-cli/test/integration/summary/summaryOption.test.ts
@@ -7,9 +7,10 @@ describe('--summary option integration', () => {
   describe('--summary option prints ', () => {
     it('prints the list of components found in arui-feather example', () => {
       // when
-      return runUXPinCodeCommand('resources/repos/arui-feather', '--summary').then((output) => {
-        // then
-        expect(output).toContain(`amount
+      return runUXPinCodeCommand('resources/repos/arui-feather', '--summary')
+        .then((output) => {
+          // then
+          expect(output).toContain(`amount
 app-content
 app-menu
 app-title
@@ -63,13 +64,15 @@ support
 textarea
 theme-provider
 user`);
-      });
+        });
     });
+
     it('prints the list of components found in nordnet-ui-kit example', () => {
       // when
-      return runUXPinCodeCommand('resources/repos/nordnet-ui-kit', '--summary').then((output) => {
-        // then
-        expect(output).toContain(`components/alert
+      return runUXPinCodeCommand('resources/repos/nordnet-ui-kit', '--summary')
+        .then((output) => {
+          // then
+          expect(output).toContain(`components/alert
 components/animate
 components/avatar
 components/badge
@@ -94,7 +97,71 @@ components/tbody\n` + // no examples
 `components/tooltip
 components/tr\n` + // no examples
 `components/ul`);
-      });
+        });
+    });
+
+    it('prints the list of components found in polaris example', () => {
+      // when
+      return runUXPinCodeCommand('resources/repos/polaris', '--summary')
+        .then((output) => {
+          // then
+          expect(output).toContain(`components/AccountConnection
+components/ActionList
+components/Avatar
+components/Badge
+components/Banner
+components/Breadcrumbs
+components/Button
+components/ButtonGroup
+components/CalloutCard
+components/Caption
+components/Card
+components/Checkbox
+components/Choice
+components/ChoiceList
+components/Collapsible
+components/ColorPicker
+components/Connected
+components/DatePicker
+components/DescriptionList
+components/DisplayText
+components/EmptyState
+components/EventListener
+components/FooterHelp
+components/FormLayout
+components/Heading
+components/Icon
+components/Image
+components/KeyboardKey
+components/KeypressListener
+components/Label
+components/Labelled
+components/Layout
+components/Link
+components/List
+components/Page
+components/PageActions
+components/Pagination
+components/Popover
+components/PositionedOverlay
+components/RadioButton
+components/ResourceList
+components/Scrollable
+components/Select
+components/SettingAction
+components/SettingToggle
+components/Stack
+components/Subheading
+components/Tabs
+components/Tag
+components/TextContainer
+components/TextField
+components/TextStyle
+components/Thumbnail
+components/Tooltip
+components/UnstyledLink
+components/VisuallyHidden`);
+        });
     });
   });
 });
