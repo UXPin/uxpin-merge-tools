cefa0644351d9f1b7dd73a15e565d19c612aea26
Michał Stocki <michal.stocki@uxpin.com>
2017-09-21 10:10:02 +0200

    10272: Minor renaming

--- packages/uxpin-code-cli/src/serialization/props/javascript/type/structure/shape/convertShapeTypeStructure.ts
+++ packages/uxpin-code-cli/src/serialization/props/javascript/type/structure/shape/convertShapeTypeStructure.ts
@@ -3,8 +3,8 @@ import { ShapeTypeStructure } from '../../../../ComponentPropertyDefinition';
 import { convertPropertyType } from '../../convertPropertyType';
 
 export function convertShapeTypeStructure(reactDocgenShape:any):ShapeTypeStructure {
-  return reduce(reactDocgenShape, (aggregator:ShapeTypeStructure, value:any, key:string) => {
-    aggregator[key] = convertPropertyType(value);
-    return aggregator;
+  return reduce(reactDocgenShape, (shape:ShapeTypeStructure, propType:any, propName:string) => {
+    shape[propName] = convertPropertyType(propType);
+    return shape;
   }, {} as ShapeTypeStructure);
 }
