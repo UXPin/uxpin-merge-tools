a6fb07ebcf7b9f42ecf9d14004216ecc64921c3b
Michał Stocki <michal.stocki@uxpin.com>
2017-09-21 10:41:10 +0200

    10272: Removed redundant output validation
    `parse` method throws inner exception when the file has no component
    so we don't need to check `isEmpty` on the parsing result.

--- packages/uxpin-code-cli/src/serialization/props/javascript/getDefaultComponentFrom.ts
+++ packages/uxpin-code-cli/src/serialization/props/javascript/getDefaultComponentFrom.ts
@@ -1,16 +1,9 @@
 import fsReadfilePromise = require('fs-readfile-promise');
-import { isEmpty } from 'lodash';
 import { parse } from 'react-docgen';
 import { ComponentDoc } from 'react-docgen-typescript/lib';
 
 export function getDefaultComponentFrom(filePath:string):Promise<ComponentDoc> {
-  return getFileContents(filePath).then((fileContents:string) => {
-    const component:ComponentDoc | undefined = parse(fileContents);
-    if (isEmpty(component)) {
-      throw new Error(`Cannot find default exported component in ${filePath}`);
-    }
-    return component;
-  });
+  return getFileContents(filePath).then(parse);
 }
 
 function getFileContents(filePath:string):Promise<string> {
