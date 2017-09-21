f3648c3b97c0528a86faf2f59ec363705d0ec163
Michał Stocki <michal.stocki@uxpin.com>
2017-09-21 09:15:42 +0200

    10272: Refactored `convertPropItemToPropertyDefinition`

--- packages/uxpin-code-cli/src/serialization/props/javascript/convertPropItemToPropertyDefinition.ts
+++ packages/uxpin-code-cli/src/serialization/props/javascript/convertPropItemToPropertyDefinition.ts
@@ -7,27 +7,37 @@ import { convertPropertyType } from './type/convertPropertyType';
 
 export function convertPropItemToPropertyDefinition(propName:string,
                                                     propItem:PropItem):Promise<PropDefinitionSerializationResult> {
-  const definition:ComponentPropertyDefinition = {
-    description: propItem.description,
-    isRequired: propItem.required,
-    name: propName,
-    type: convertPropertyType(propItem.type),
-  };
+  return getDefaultValue(propName, propItem).then(({ partialDefinition, warnings }) => {
+    const definition:ComponentPropertyDefinition = {
+      ...partialDefinition,
+      description: propItem.description,
+      isRequired: propItem.required,
+      name: propName,
+      type: convertPropertyType(propItem.type),
+    };
+    return { definition, warnings };
+  });
+}
 
-  if (propItem.defaultValue) {
-    return parseValue(propItem.defaultValue.value).then((value:any) => {
-      definition.defaultValue = { value };
-      return { definition, warnings: [] };
-    }).catch((error:Error) => {
-      const warning:WarningDetails = {
-        message: `Cannot compute default value for property \`${propName}\`.`,
-        originalError: error,
-      };
-      return Promise.resolve({
-        definition,
-        warnings: [warning],
+function getDefaultValue(propName:string, propItem:PropItem):Promise<DefaultValueParsingResult> {
+  const partialDefinition:Pick<ComponentPropertyDefinition, 'defaultValue'> = {};
+  const warnings:WarningDetails[] = [];
+  return new Promise((resolve) => {
+    if (propItem.defaultValue) {
+      return parseValue(propItem.defaultValue.value).then((value:any) => {
+        partialDefinition.defaultValue = { value };
+      }).catch((originalError:Error) => {
+        warnings.push({ originalError, message: `Cannot compute default value for property \`${propName}\`.` });
+      }).then(() => {
+        resolve({ partialDefinition, warnings });
       });
-    });
-  }
-  return Promise.resolve({ definition, warnings: [] });
+    } else {
+      resolve({ partialDefinition, warnings });
+    }
+  });
+}
+
+interface DefaultValueParsingResult {
+  partialDefinition:Pick<ComponentPropertyDefinition, 'defaultValue'>;
+  warnings:WarningDetails[];
 }
