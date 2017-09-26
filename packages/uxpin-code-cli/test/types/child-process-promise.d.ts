82db0a277aaaf609cf2ceccd61d2e4308a07424d
Michał Stocki <michal.stocki@uxpin.com>
2017-09-26 10:32:09 +0200

    10272: Updated dump of `arui-feather` example with support for `arrayOf`

--- packages/uxpin-code-cli/test/types/child-process-promise.d.ts
+++ packages/uxpin-code-cli/test/types/child-process-promise.d.ts
@@ -1,7 +1,10 @@
 // More API here: https://www.npmjs.com/package/child-process-promise
 
+import { ExecOptions, SpawnOptions } from 'child_process';
+
 declare module 'child-process-promise' {
-  export function exec(command:string):Promise<CommandResult>;
+  export function exec(command:string, options?:ExecOptions):Promise<CommandResult>;
+  export function spawn(command:string, options?:SpawnOptions):Promise<CommandResult>;
 
   export interface CommandResult {
     stdout:string;
--- packages/uxpin-code-cli/test/utils/command/runUXPinCodeCommand.ts
+++ packages/uxpin-code-cli/test/utils/command/runUXPinCodeCommand.ts
@@ -1,3 +1,4 @@
+import { ExecOptions } from 'child_process';
 import { join } from 'path';
 import { getRandomString } from './getRandomString';
 import { runCommand } from './runCommand';
@@ -8,9 +9,8 @@ const uxPinPath:string = join(packageRootDir, 'bin/uxpin-code');
 
 export function runUXPinCodeCommand(workingDir:string, options?:string):Promise<string> {
   const absoluteWorkingDir:string = getAbsoluteWorkingDir(workingDir);
-
   const coverageCommand:string = `${nycPath} ${getNycOptions()}`;
-  return runCommand(`cd ${absoluteWorkingDir} && ${coverageCommand} ${uxPinPath} ${options}`);
+  return runCommand(`cd ${absoluteWorkingDir} && ${coverageCommand} ${uxPinPath} ${options}`, getExecOptions());
 }
 
 function getAbsoluteWorkingDir(pathRelativeToTestDir:string):string {
@@ -28,4 +28,10 @@ function getNycOptions():string {
 --report-dir="${coverageDir}" \
 --reporter=clover \
 --extension=".ts"`;
+}
+
+function getExecOptions():ExecOptions {
+  const bytesPerKByte:number = 1024;
+  const maxStdOutBufferSizeInKB:number = 500;
+  return { maxBuffer: bytesPerKByte * maxStdOutBufferSizeInKB };
 }
