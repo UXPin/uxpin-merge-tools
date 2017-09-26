5654aba8b35b5ed1c2c24905c786e0a9a06ad0af
Michał Stocki <michal.stocki@uxpin.com>
2017-09-26 11:14:08 +0200

    10272: Fixed compilation in `getFileString` test. Updated tslint config

--- packages/uxpin-code-cli/test/spec/building/library/getFileString.test.ts
+++ packages/uxpin-code-cli/test/spec/building/library/getFileString.test.ts
@@ -1,15 +1,20 @@
 import { getFileString } from '../../../../src/building/library/getFileString';
-import { ComponentInfo } from '../../../../src/components/ComponentInfo';
+import { ComponentImplementationInfo, ComponentInfo } from '../../../../src/components/ComponentInfo';
 
 describe('getFileString', () => {
+
+  const implementation:ComponentImplementationInfo = { path: '', framework: 'reactjs', lang: 'javascript' };
+
   it('returns content of library file for list of ComponentInfo', () => {
     const componentInfos:ComponentInfo[] = [
       {
         dirPath: 'components/button',
+        implementation,
         name: 'button',
       },
       {
         dirPath: 'components/button-list',
+        implementation,
         name: 'button-list',
       },
     ];
@@ -32,10 +37,12 @@ export {
     const componentInfos:ComponentInfo[] = [
       {
         dirPath: 'components/button',
+        implementation,
         name: 'button',
       },
       {
         dirPath: 'components/button-list',
+        implementation,
         name: 'button-list',
       },
     ];
