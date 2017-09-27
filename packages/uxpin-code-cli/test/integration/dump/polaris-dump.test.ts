0a70e1a369ab672ef2a94e0c733ae23b7d987c6d
Michał Stocki <michal.stocki@uxpin.com>
2017-09-27 11:07:12 +0200

    10272: Added integration test for --dump for the polaris repository

---
+++ packages/uxpin-code-cli/test/integration/dump/polaris-dump.test.ts
@@ -0,0 +1,15 @@
+import { runUXPinCodeCommand } from '../../utils/command/runUXPinCodeCommand';
+import { setTimeoutBeforeAll } from '../../utils/command/setTimeoutBeforeAll';
+
+const CURRENT_TIMEOUT:number = 60000;
+setTimeoutBeforeAll(CURRENT_TIMEOUT);
+
+describe('The --dump option', () => {
+  it('run for the polaris repository, prints the JSON describing the full repository', () => {
+    // when
+    return runUXPinCodeCommand('resources/repos/polaris', '--dump').then((consoleOutput) => {
+      // then
+      expect(consoleOutput).toMatchSnapshot();
+    });
+  });
+});
