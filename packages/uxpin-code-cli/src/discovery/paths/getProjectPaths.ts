4f9110a6b59b759d3a114d37a1bcd72f3f57f2df
Michał Stocki <michal.stocki@uxpin.com>
2017-09-27 09:38:49 +0200

    10272: Refactored `getProjectPaths`.

--- packages/uxpin-code-cli/src/discovery/paths/getProjectPaths.ts
+++ packages/uxpin-code-cli/src/discovery/paths/getProjectPaths.ts
@@ -1,21 +1,14 @@
 import isDirPromise = require('is-dir-promise');
+import pAny = require('p-any');
 import { join, relative } from 'path';
 import { PATHS } from './paths';
 import { ProjectPaths } from './ProjectPaths';
 
 export function getProjectPaths():Promise<ProjectPaths> {
   const cwd:string = process.cwd();
-  const paths:string[] = PATHS.map((directories) => join(cwd, ...directories));
-
-  return Promise.all(paths.map((dirPath) => isDirPromise(dirPath).then(() => true).catch(() => false)))
-    .then((isDirectoryList) => {
-      const found:string | undefined = paths.find((path, index) => isDirectoryList[index]);
-      if (found) {
-        return {
-          componentsDirPath: relative(cwd, found),
-          projectRoot: cwd,
-        };
-      }
-      throw new Error('Unable to locate components source directory');
-    });
+  return pAny(PATHS
+    .map((directories) => join(cwd, ...directories))
+    .map((path) => isDirPromise(path).then(() => path)))
+    .then((foundPath) => ({ componentsDirPath: relative(cwd, foundPath), projectRoot: cwd }))
+    .catch(() => Promise.reject(new Error('Unable to locate components source directory')));
 }
