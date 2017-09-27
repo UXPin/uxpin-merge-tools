99e7c344bfdab8c6a7006e1ae1f8faec2b348cf4
Michał Stocki <michal.stocki@uxpin.com>
2017-09-27 10:53:44 +0200

    10272: Refactored `getProjectPaths` to return always the first existing path

--- packages/uxpin-code-cli/src/discovery/paths/getProjectPaths.ts
+++ packages/uxpin-code-cli/src/discovery/paths/getProjectPaths.ts
@@ -1,14 +1,14 @@
-import isDirPromise = require('is-dir-promise');
-import pAny = require('p-any');
+import locatePath = require('locate-path');
 import { join, relative } from 'path';
 import { PATHS } from './paths';
 import { ProjectPaths } from './ProjectPaths';
 
 export function getProjectPaths():Promise<ProjectPaths> {
   const cwd:string = process.cwd();
-  return pAny(PATHS
-    .map((directories) => join(cwd, ...directories))
-    .map((path) => isDirPromise(path).then(() => path)))
-    .then((foundPath) => ({ componentsDirPath: relative(cwd, foundPath), projectRoot: cwd }))
-    .catch(() => Promise.reject(new Error('Unable to locate components source directory')));
+  return locatePath(PATHS.map((directories) => join(cwd, ...directories))).then((foundPath) => {
+    if (foundPath) {
+      return ({ componentsDirPath: relative(cwd, foundPath), projectRoot: cwd });
+    }
+    throw new Error('Unable to locate components source directory');
+  });
 }
