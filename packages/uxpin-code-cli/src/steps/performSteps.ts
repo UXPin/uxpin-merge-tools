1613f5c822c924386b97731f3828112f80f05737
Michał Stocki <michal.stocki@uxpin.com>
2017-10-03 12:29:55 +0200

    10272: Refactored `performSteps` function
    (cherry picked from commit b60412d)

--- packages/uxpin-code-cli/src/steps/performSteps.ts
+++ packages/uxpin-code-cli/src/steps/performSteps.ts
@@ -1,3 +1,4 @@
+import pReduce = require('p-reduce');
 import { stringifyWarnings } from '../common/warning/stringifyWarnings';
 import { tapPromise } from '../utils/promise/tapPromise';
 import { buildDesignSystem } from './building/buildDesignSystem';
@@ -11,32 +12,38 @@ import { printDump } from './serialization/printDump';
 
 export function performSteps(args:ProgramArgs):Promise<any> {
   const { dump, summary, target, webpackConfig, wrapper } = args;
-
-  if (summary) {
-    return getDesignSystemComponentInfos()
-      .then(getDesignSystemSummary)
-      .then(console.log)
-      .catch(logError);
-  }
-  if (dump) {
-    return getDesignSystemComponentInfos()
-      .then(printDump)
-      .catch(logError);
-  }
-
   const buildOptions:BuildOptions = { target, webpackConfigPath: webpackConfig, wrapperPath: wrapper };
 
-  return getDesignSystemComponentInfos()
-    .then(tapPromise((infos) => buildDesignSystem(infos, buildOptions)))
-    .then(tapPromise((infos) => console.log(getDesignSystemSummary(infos))))
-    .then(tapPromise(printSerializationWarnings))
-    .catch(logError);
+  const steps:Step[] = [
+    { exec: (infos) => buildDesignSystem(infos, buildOptions), shouldRun: !dump && !summary },
+    { exec: printDump, shouldRun: dump },
+    { exec: printSummary, shouldRun: !dump },
+    { exec: printSerializationWarnings, shouldRun: !dump },
+  ];
+
+  const stepFunctions:StepExecutor[] = steps.filter((step) => step.shouldRun).map((step) => tapPromise(step.exec));
+
+  return getDesignSystemComponentInfos().then((infos:ComponentInfo[]) => {
+    return pReduce(stepFunctions, (reduceInfos, step) => step(reduceInfos), infos);
+  }).catch(logError);
+
 }
 
 function printSerializationWarnings(infos:ComponentInfo[]):Promise<any> {
   return getDesignSystemMetadata(infos).then(({ warnings }) => console.log(stringifyWarnings(warnings)));
 }
 
+function printSummary(infos:ComponentInfo[]):void {
+  console.log(getDesignSystemSummary(infos));
+}
+
 function logError(errorMessage:string):void {
   console.log('ERROR:', errorMessage);
+}
+
+type StepExecutor = (infos:ComponentInfo[]) => Promise<ComponentInfo[]>;
+
+interface Step {
+  shouldRun:boolean;
+  exec:(infos:ComponentInfo[]) => any;
 }
