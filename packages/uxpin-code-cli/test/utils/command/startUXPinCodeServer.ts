a0e07b213dbc427148fb28fcc4e881aa23709640
Michał Stocki <michal.stocki@uxpin.com>
2017-10-19 15:55:07 +0200

    11041: `startUXPinCodeServer` split into smaller functions

--- packages/uxpin-code-cli/test/utils/command/startUXPinCodeServer.ts
+++ packages/uxpin-code-cli/test/utils/command/startUXPinCodeServer.ts
@@ -8,15 +8,23 @@ export function startUXPinCodeServer(workingDir:string, opts?:string):Promise<()
     const uxpinCommandOptions:string = ['server', opts].filter(Boolean).join(' ');
     const command:string = buildCommand(workingDir, uxpinCommandOptions);
     const subprocess:ChildProcess = exec(command, getExecOptions());
-    const serverStarted:RegExp = new RegExp(SERVER_SUCCESS_MESSAGE);
-    subprocess.stdout.on('data', (data) => {
-      if (data.toString().match(serverStarted)) {
-        resolve(() => subprocess.kill());
-      }
-    });
-    let errorOut:string = '';
-    subprocess.stderr.on('data', (data) => errorOut += data);
-    subprocess.on('close', () => reject(errorOut));
-    subprocess.on('error', (data) => reject(data));
+    onServerReady(subprocess, () => resolve(() => subprocess.kill()));
+    onFailure(subprocess, (error) => reject(error));
   });
+}
+
+function onServerReady(subprocess:ChildProcess, callback:() => void):void {
+  const serverStarted:RegExp = new RegExp(SERVER_SUCCESS_MESSAGE);
+  subprocess.stdout.on('data', (data) => {
+    if (data.toString().match(serverStarted)) {
+      callback();
+    }
+  });
+}
+
+function onFailure(subprocess:ChildProcess, callback:(error:any) => void):void {
+  let errorOut:string = '';
+  subprocess.stderr.on('data', (data) => errorOut += data);
+  subprocess.on('close', () => callback(errorOut));
+  subprocess.on('error', (data) => callback(data));
 }
