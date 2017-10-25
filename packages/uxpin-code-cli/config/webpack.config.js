ea552a21aa8c91d576e9f5317cfae4b568379f84
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2017-10-25 14:33:10 +0200

    11051: Bundle server styleguide app

---
+++ packages/uxpin-code-cli/config/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: './src/steps/styleGuide/App.js',
+  output: {
+    filename: './dist/bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+};
