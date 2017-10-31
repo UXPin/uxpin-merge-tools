9b4a03847102b7893e2aee950b7cd16967d3edb5
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2017-10-31 16:55:39 +0100

    11051: Remove unused getHtmlString file

--- packages/uxpin-code-cli/src/steps/styleGuide/getHtmlString.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { renderToString } from 'react-dom/server';
-
-// tslint:disable:variable-name
-export function getHtmlString<Props>(Component:React.SFC<Props>|React.ComponentClass<Props>,
-  props:Partial<Props> = {}):string {
-
-  return renderToString(<Component {...props} />);
-}
