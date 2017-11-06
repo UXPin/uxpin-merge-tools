44ac5e6c26ebd3576080ce00ced93bd9296f8abc
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2017-11-06 09:36:42 +0100

    11051: Fix copyRequiredFiles

--- packages/uxpin-code-cli/src/debug/server/copyRequiredFiles.ts
+++ packages/uxpin-code-cli/src/debug/server/copyRequiredFiles.ts
@@ -3,7 +3,7 @@ import { join, resolve } from 'path';
 import { STYLEGUIDE_BUNDLE_FILE } from '../../steps/building/config/getConfig';
 
 export function copyRequiredFiles(root:string):Promise<string> {
-  const source:string = resolve('../../../../dist', STYLEGUIDE_BUNDLE_FILE);
+  const source:string = resolve(__dirname, '../../../dist', STYLEGUIDE_BUNDLE_FILE);
   const target:string = join(root, STYLEGUIDE_BUNDLE_FILE);
   return copy(source, target).then(() => target);
 }
