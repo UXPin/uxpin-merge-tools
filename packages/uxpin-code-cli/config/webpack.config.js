33daa2c34f8ab81982f8750792179a5982379dc7
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2017-11-06 10:18:06 +0100

    11051: Revert moving config

---
+++ packages/uxpin-code-cli/config/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: './src/debug/server/frontEnd/App.js',
+  output: {
+    filename: './dist/styleguide.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+};
