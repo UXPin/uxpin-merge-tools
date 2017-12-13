f33a2f4ba6286466b7d6bf0daf53394d9a51ca38
Michał Stocki <michal.stocki@uxpin.com>
2017-12-13 15:19:28 +0100

    add-mineral-ui: Add support for `.js` extension for React components

--- packages/uxpin-code-cli/src/steps/discovery/component/implementation/strategies/getJSImplementationInfo.ts
+++ packages/uxpin-code-cli/src/steps/discovery/component/implementation/strategies/getJSImplementationInfo.ts
@@ -1,15 +1,28 @@
+import pAny = require('p-any');
 import { join, relative } from 'path';
 import { isFile } from '../../../../../utils/fs/isFile';
 import { ComponentImplementationInfo } from '../../ComponentInfo';
 import { ComponentPaths } from '../../ComponentPaths';
 
 export function getJSImplementationInfo(paths:ComponentPaths):Promise<ComponentImplementationInfo> {
-  const { projectRoot, componentDirPath, componentDirName } = paths;
-  const absoluteComponentPath:string = join(projectRoot, componentDirPath, `${componentDirName}.jsx`);
-  const info:ComponentImplementationInfo = {
+  const jsPath:string = absoluteComponentPath(paths, 'js');
+  const jsxPath:string = absoluteComponentPath(paths, 'jsx');
+  return pAny([isComponentFile(jsxPath), isComponentFile(jsPath)])
+    .then((foundPath) => getComponentInfo(paths, foundPath));
+}
+
+function absoluteComponentPath({ projectRoot, componentDirPath, componentDirName }:ComponentPaths, ext:string):string {
+  return join(projectRoot, componentDirPath, `${componentDirName}.${ext}`);
+}
+
+function isComponentFile(path:string):Promise<string> {
+  return isFile(path).then(() => path);
+}
+
+function getComponentInfo({ projectRoot }:ComponentPaths, componentPath:string):ComponentImplementationInfo {
+  return {
     framework: 'reactjs',
     lang: 'javascript',
-    path: relative(projectRoot, absoluteComponentPath),
+    path: relative(projectRoot, componentPath),
   };
-  return isFile(absoluteComponentPath).then(() => info);
 }
