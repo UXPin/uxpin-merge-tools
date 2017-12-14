6bc384c16881dfdf4bfdb6a52b119d54283c1913
Michał Stocki <michal.stocki@uxpin.com>
2017-12-14 14:42:03 +0100

    12091: Add typings for babylon AST output

---
+++ packages/uxpin-code-cli/src/types/babylon-ast.d.ts
@@ -0,0 +1,81 @@
+export type FlowType = FlowArray
+  | FlowTypeSignature<'function'>
+  | FlowTypeSignature<'object'>
+  | FlowPrimitiveType
+  | FlowUnionType
+  | FlowLiteralType
+  | FlowUnknownType;
+
+export interface FlowArray {
+  name:'Array';
+  elements:FlowType[];
+  raw:string;
+}
+
+export interface FlowTypeSignature<T extends keyof FlowTypeSignaturesMap> {
+  name:'signature';
+  type:T;
+  raw:string;
+  signature:FlowTypeSignaturesMap[T];
+}
+
+export interface FlowTypeSignaturesMap {
+  function:FlowFunctionSignature;
+  object:FlowObjectSignature;
+}
+
+export interface FlowFunctionSignature {
+  arguments:FlowFunctionSignatureArgumentType[];
+  return:FlowType;
+}
+
+export interface FlowFunctionSignatureArgumentType {
+  name:string;
+  type:FlowType;
+}
+
+export interface FlowObjectSignature {
+  properties:FlowObjectSignatureProperty[];
+}
+
+export interface FlowObjectSignatureProperty {
+  key:string;
+  value:FlowObjectSignaturePropertyValueType;
+}
+
+type FlowObjectSignaturePropertyValueType = FlowType & {
+  required:boolean;
+};
+
+export interface FlowUnionType {
+  name:'union';
+  raw:string;
+  elements:FlowType[];
+}
+
+export interface FlowLiteralType {
+  name:'literal';
+  value:string;
+}
+
+export interface FlowUnknownType {
+  name:string;
+}
+
+export interface FlowPrimitiveType {
+  name:FlowPrimitiveTypeName;
+}
+
+type FlowPrimitiveTypeName = 'any'
+  | 'bool'
+  | 'boolean'
+  | 'empty'
+  | 'false'
+  | 'mixed'
+  | 'null'
+  | 'number'
+  | 'static'
+  | 'string'
+  | 'true'
+  | 'typeof'
+  | 'void';
