247d159e4b99ae0cbedb4a8e64ba948e0f46038c
Michał Stocki <michal.stocki@uxpin.com>
2017-12-19 11:39:51 +0100

    12091: Removed warning
    (cherry picked from commit 4d6eadc)

--- packages/uxpin-code-cli/src/steps/serialization/component/implementation/javascript/type/flow/strategy/convertArrayFlowType.ts
+++ packages/uxpin-code-cli/src/steps/serialization/component/implementation/javascript/type/flow/strategy/convertArrayFlowType.ts
@@ -3,7 +3,6 @@ import { PropertyType } from '../../../../ComponentPropertyDefinition';
 import { convertFlowPropertyType } from '../convertFlowPropertyType';
 
 export function convertArrayFlowType(flowType:FlowArray):PropertyType<'typedArray'> {
-  // @todo: Warning: tuple is not currently supported
   return {
     name: 'typedArray',
     structure: {
