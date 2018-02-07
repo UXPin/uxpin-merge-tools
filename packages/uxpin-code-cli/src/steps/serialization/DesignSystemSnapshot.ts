e138546adbc2d0643c44d8d9a08fd1889f192ce8
Michał Stocki <michal.stocki@uxpin.com>
2018-02-07 14:50:18 +0100

    12920: Described missing data structures in DesignSystemSnapshot object

--- packages/uxpin-code-cli/src/steps/serialization/DesignSystemSnapshot.ts
+++ packages/uxpin-code-cli/src/steps/serialization/DesignSystemSnapshot.ts
@@ -3,4 +3,27 @@ import { ComponentDefinition } from './component/ComponentDefinition';
 export interface DesignSystemSnapshot {
   name:string;
   components:ComponentDefinition[];
+  vcs?:VCSDetails;
+}
+
+export interface VCSDetails {
+  commitHash:string;
+  branchName:string;
+  movedFiles:MovedFiles;
+  tags:VCSTag[];
+}
+
+export interface MovedFiles {
+  diffSourceCommitHash:string;
+  components:MovedFilePathsMap;
+  presets:MovedFilePathsMap;
+}
+
+export interface MovedFilePathsMap {
+  [sourcePath:string]:string;
+}
+
+export interface VCSTag {
+  name:string;
+  commitHash:string;
 }
