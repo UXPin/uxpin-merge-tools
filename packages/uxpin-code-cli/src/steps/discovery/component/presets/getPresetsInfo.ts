0c4a0c7a0421c4ec0a05be20cb1da74ecd5ad76e
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2018-02-15 00:09:19 +0100

    t12924: Fix presets discovery

--- packages/uxpin-code-cli/src/steps/discovery/component/presets/getPresetsInfo.ts
+++ packages/uxpin-code-cli/src/steps/discovery/component/presets/getPresetsInfo.ts
@@ -1,8 +1,7 @@
 import pReduce = require('p-reduce');
-import { join } from 'path';
+import { join, relative } from 'path';
 import { isFile } from '../../../../utils/fs/isFile';
 import { readDir } from '../../../../utils/fs/readDir';
-import { tapPromise } from '../../../../utils/promise/tapPromise';
 import { ComponentPresetsInfo } from '../ComponentInfo';
 import { ComponentPaths } from '../ComponentPaths';
 import { filterPresets } from './filterPresets';
@@ -11,18 +10,34 @@ import { sortPresets } from './sortPresets';
 const PRESETS_DIR:string = 'presets';
 
 export function getPresetsInfo(componentPaths:ComponentPaths):Promise<ComponentPresetsInfo> {
-  return getFiles(join(componentPaths.componentsDirPath, PRESETS_DIR))
+  return getFilePaths(join(componentPaths.projectRoot, componentPaths.componentDirPath, PRESETS_DIR))
+    .then((paths) => getRelativePaths(componentPaths.projectRoot, paths))
     .then(filterPresets)
     .then(sortPresets)
     .then((paths) => ({ paths }));
 }
 
-function getFiles(path:string):Promise<string[]> {
-  return readDir(path)
-    .then((content) => content.map(tapPromise(isFile)))
-    .then((files) => pReduce(files, (result, file) => {
-      result.push(file);
-      return result;
-    }, [] as string[]))
+function getFilePaths(dirPath:string):Promise<string[]> {
+  return readDir(dirPath)
+    .then((content) => content.map((fileName) => join(dirPath, fileName)))
+    .then(skipDirectories)
     .catch(() => []);
+}
+
+function skipDirectories(paths:string[]):Promise<string[]> {
+  return pReduce(paths.map(getFilePath), (result, filePath) => {
+    if (filePath) {
+      result.push(filePath);
+    }
+
+    return result;
+  }, [] as string[]);
+}
+
+function getFilePath(path:string):Promise<string|null> {
+  return isFile(path).then((value) => value ? path : null).catch(() => null);
+}
+
+function getRelativePaths(fromPath:string, paths:string[]):string[] {
+  return paths.map((path) => relative(fromPath, path));
 }
