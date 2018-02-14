dc9cf16e20369516abb4b91d062532ba9915c69b
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2018-02-15 00:11:28 +0100

    t12924: Include presets in summary

--- packages/uxpin-code-cli/test/integration/summary/polaris-summary.test.ts
+++ packages/uxpin-code-cli/test/integration/summary/polaris-summary.test.ts
@@ -15,242 +15,302 @@ describe('--summary option integration', () => {
           expect(output).toContain(`AccountConnection
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 ActionList
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Avatar
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Badge
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Banner
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Breadcrumbs
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 Button
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 ButtonGroup
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 CalloutCard
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Caption
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Card
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Checkbox
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Choice
     📜 documentation: ✔
     💡 examples: ✘
+    🎛  presets: ✘
 
 ChoiceList
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Collapsible
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 ColorPicker
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Connected
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 DatePicker
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 DescriptionList
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 DisplayText
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 EmptyState
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 EventListener
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 FooterHelp
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 FormLayout
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Heading
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Icon
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Image
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 KeyboardKey
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 KeypressListener
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 labelID
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 Labelled
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 Layout
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Link
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 List
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Page
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 PageActions
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Pagination
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Popover
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 PositionedOverlay
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 RadioButton
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 ResourceList
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Scrollable
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Select
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 SettingAction
     📜 documentation: ✔
     💡 examples: ✘
+    🎛  presets: ✘
 
 SettingToggle
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 SkeletonBodyText
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 SkeletonDisplayText
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 SkeletonPage
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Spinner
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Stack
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Subheading
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Tabs
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Tag
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 TextContainer
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 TextField
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 TextStyle
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Thumbnail
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 Tooltip
     📜 documentation: ✔
     💡 examples: ✔
+    🎛  presets: ✘
 
 UnstyledLink
     📜 documentation: ✘
     💡 examples: ✘
+    🎛  presets: ✘
 
 VisuallyHidden
     📜 documentation: ✔
-    💡 examples: ✔`);
+    💡 examples: ✔
+    🎛  presets: ✘`);
         });
     });
   });
