2ca88f1f6ea601533bb44e050076e6eb018c9d82
Michał Stocki <michal.stocki@uxpin.com>
2018-04-30 18:37:09 +0200

    11939: Added `using` util for parametrized tests

---
+++ packages/uxpin-code-cli/test/utils/using.ts
@@ -0,0 +1,15 @@
+interface Using<T> {
+  describe(description:string, testSuite:(testCase:T) => void):void;
+}
+
+export function using<T>(cases:T[]):Using<T> {
+  return {
+    describe(description:string, testSuite:(testCase:T) => void):void {
+      describe(description, () => {
+        cases.forEach((testCase:any) => {
+          testSuite(testCase);
+        });
+      });
+    },
+  };
+}
