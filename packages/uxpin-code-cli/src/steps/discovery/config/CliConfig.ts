5d39da5c6f616aa6ff99f2a0a5d558755ab510b6
Michał Stocki <michal.stocki@uxpin.com>
2018-04-30 16:17:39 +0200

    11939: Created interface for config file

---
+++ packages/uxpin-code-cli/src/steps/discovery/config/CliConfig.ts
@@ -0,0 +1,29 @@
+export interface CliConfig {
+  components:ComponentsConfig;
+}
+
+export interface ComponentsConfig {
+  categories:CategoryConfig[];
+}
+
+export interface CategoryConfig {
+  name:string;
+  /**
+   * single glob pattern or list of glob patters of component files to be included in the category
+   *
+   * @example
+   * multiple glob patters matching particular component files:
+   * ```
+   * include: [
+   *   "src/patterns/Card/Card.jsx",
+   *   "src/patterns/Modal/Modal.jsx",
+   * ],
+   * ```
+   * @example
+   * single glob pattern matching all component in subdirectories in a directory:
+   * ```
+   * include: "src/components/*\/*.js",
+   * ```
+   */
+  include:string|string[];
+}
