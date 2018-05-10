cd7f85c0c60a68d9ea12e33dd4991aab7f54d66c
Michał Stocki <michal.stocki@uxpin.com>
2018-05-10 12:42:14 +0200

    14220: Fixed MineralUI e2e test

--- packages/uxpin-code-cli/test/integration/server/mineral-ui-server.test.ts
+++ packages/uxpin-code-cli/test/integration/server/mineral-ui-server.test.ts
@@ -11,7 +11,6 @@ describe('server run in mineral-ui', () => {
   let chromeless:Chromeless<any>;
 
   setupDebugServerTest({
-    env: { UXPIN_BUILD: 'true' },
     projectPath: 'resources/repos/mineral-ui',
     serverCmdArgs: [
       '--webpack-config "./webpack.config.js"',
@@ -23,11 +22,11 @@ describe('server run in mineral-ui', () => {
     // given
     const expectedDetails = { // tslint:disable-line
       itemContents: 'Danger variant',
-      textColor: 'rgb(219, 41, 41)',
+      textColor: 'rgb(222, 27, 27)',
     };
 
     const getComponentDetails:() => typeof expectedDetails = () => {
-      const dangerItemContentSelector:string = '#dropdown-1-menuItem-4 > span > span > span:first-child';
+      const dangerItemContentSelector:string = '#dropdown-1-item-2 > span > span > span:first-child';
       const dropdownContentItem:HTMLElement = document.querySelector(dangerItemContentSelector) as HTMLElement;
       const dropdownItemStyles:CSSStyleDeclaration = getComputedStyle(dropdownContentItem);
       const itemContents:string = dropdownContentItem.innerText as string;
@@ -38,9 +37,11 @@ describe('server run in mineral-ui', () => {
     };
 
     // when
-    await chromeless.wait('#dropdown-1 button', CURRENT_TIMEOUT);
-    await chromeless.click('#dropdown-1 button');
-    const scratchedDetails:any = await chromeless.evaluate(getComponentDetails);
+    const scratchedDetails:any = await chromeless
+      .wait('#dropdown-1 button', CURRENT_TIMEOUT)
+      .scrollToElement('#dropdown-1 button')
+      .click('#dropdown-1 button')
+      .evaluate(getComponentDetails);
 
     // then
     expect(scratchedDetails).toEqual(expectedDetails);
