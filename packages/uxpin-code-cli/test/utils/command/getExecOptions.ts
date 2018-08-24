ec4ed275232bb0f8a8f4cd3921f48c06327521b1
Michał Stocki <michal.stocki@uxpin.com>
2018-08-24 12:20:32 +0200

    Updated revision of Mineral UI Fork used in tests, regenerated dump snapshot

--- packages/uxpin-code-cli/test/utils/command/getExecOptions.ts
+++ packages/uxpin-code-cli/test/utils/command/getExecOptions.ts
@@ -2,6 +2,6 @@ import { ExecOptions } from 'child_process';
 
 export function getExecOptions():ExecOptions {
   const bytesPerKByte:number = 1024;
-  const maxStdOutBufferSizeInKB:number = 500;
+  const maxStdOutBufferSizeInKB:number = 1000;
   return { maxBuffer: bytesPerKByte * maxStdOutBufferSizeInKB };
 }
