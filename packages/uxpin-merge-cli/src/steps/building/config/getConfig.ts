27f25b43ade4a4cbff94a9e067d5aedecb9ea256
Lukasz Wroblewski <lukasz.wroblewski@uxpin.com>
2018-10-11 12:55:51 +0200

    16690 - missing typing

--- packages/uxpin-merge-cli/src/steps/building/config/getConfig.ts
+++ packages/uxpin-merge-cli/src/steps/building/config/getConfig.ts
@@ -3,7 +3,7 @@ import { Configuration } from 'webpack';
 import { smart } from 'webpack-merge';
 
 export const DEBUG_APP_BUNDLED_FILE:string = 'index.js';
-export const TEMP_DIR_NAME = '.uxpin-merge';
+export const TEMP_DIR_NAME:string = '.uxpin-merge';
 export const TEMP_DIR_PATH:string = `./${TEMP_DIR_NAME}`;
 export const LIBRARY_INPUT_PATH:string = `${TEMP_DIR_PATH}/components.js`;
 export const LIBRARY_METADATA_PATH:string = `${TEMP_DIR_PATH}/designsystem.json`;
