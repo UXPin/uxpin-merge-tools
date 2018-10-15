b0a8fa08a6a2e190cf5c157112009a7f4cb240c6
Michał Stocki <michal.stocki@uxpin.com>
2018-10-15 10:57:46 +0200

    16511: seven-line HTTP server

--- packages/uxpin-merge-cli/src/steps/experimentation/server/startExperimentationServer.ts
+++ packages/uxpin-merge-cli/src/steps/experimentation/server/startExperimentationServer.ts
@@ -1,8 +1,18 @@
+import { createServer, Server } from 'http';
+import { OK } from 'http-status-codes';
+
 export async function startExperimentationServer(options:ExperimentationServerOptions):Promise<void> {
-  return;
+  const server:Server = createServer((request, response) => {
+    response.writeHead(OK, {
+      'Content-Type': 'application/json',
+    });
+    response.end(JSON.stringify({ hello: 'World!' }));
+  });
+  server.listen(options.port);
 }
 
 export interface ExperimentationServerOptions {
+  uxpinDomain:string;
   port:number;
   uxpinDirPath:string;
 }
