2d818730bfbf9f02b89964e23066d3d6cf3f503c
Michał Stocki <michal.stocki@uxpin.com>
2018-10-15 11:56:23 +0200

    16511: Added `uxpinDomain` to CLI options

--- packages/uxpin-merge-cli/src/program/command/experimentation/getExperimentationServerCommandSteps.ts
+++ packages/uxpin-merge-cli/src/program/command/experimentation/getExperimentationServerCommandSteps.ts
@@ -1,23 +1,22 @@
-import { join } from 'path';
-import { BuildOptions } from '../../../steps/building/BuildOptions';
-import { TEMP_DIR_PATH } from '../../../steps/building/config/getConfig';
 import { startExperimentationServer } from '../../../steps/experimentation/server/startExperimentationServer';
+import { ExperimentProgramArgs } from '../../args/ProgramArgs';
+import { getTempDirPath } from '../../args/providers/paths/getTempDirPath';
 import { DSMetadata } from '../../DSMeta';
-import { ExperimentProgramArgs } from '../../ProgramArgs';
-import { getBuildOptions } from '../push/getBuildOptions';
 import { Step } from '../Step';
 
 export function getExperimentationServerCommandSteps(args:ExperimentProgramArgs):Step[] {
   return [
-    { exec: thunkStartExperimentationServer(getBuildOptions(args), args.port), shouldRun: true },
+    { exec: thunkStartExperimentationServer(args), shouldRun: true },
   ];
 }
 
-function thunkStartExperimentationServer(buildOptions:BuildOptions, port:number):(ds:DSMetadata) => Promise<any> {
+function thunkStartExperimentationServer(args:ExperimentProgramArgs):(ds:DSMetadata) => Promise<any> {
   return () => {
+    const { port, uxpinDomain } = args;
     return startExperimentationServer({
       port,
-      uxpinDirPath: join(buildOptions.projectRoot, TEMP_DIR_PATH),
+      uxpinDirPath: getTempDirPath(args),
+      uxpinDomain,
     });
   };
 }
