fb31374a25c261345d722dc8ee0960d609561229
Michał Stocki <michal.stocki@uxpin.com>
2018-10-15 22:23:55 +0200

    16511: Reduce number of command executions for `--help` tests

--- packages/uxpin-merge-cli/test/integration/help/dumpCommandHelp.test.ts
+++ packages/uxpin-merge-cli/test/integration/help/dumpCommandHelp.test.ts
@@ -1,20 +1,21 @@
 import { runUXPinMergeCommand } from '../../utils/command/runUXPinMergeCommand';
 
 describe('--help option for `dump` command', () => {
-  it('it prints description for dump command', async () => {
-    // when
-    const output:string = await runUXPinMergeCommand({ params: ['dump', '--help'] });
 
+  let output:string;
+
+  beforeAll(async () => {
+    output = await runUXPinMergeCommand({ params: ['dump', '--help'] });
+  });
+
+  it('it prints description for dump command', () => {
     // then
     expect(output).toContain(
       'Shows all information (in JSON) about the design system repository and NOT send to UXPin',
     );
   });
 
-  it('it prints help for --cwd <path> option', async () => {
-    // when
-    const output:string = await runUXPinMergeCommand({ params: ['dump', '--help'] });
-
+  it('it prints help for --cwd <path> option', () => {
     // then
     expect(output).toMatch(/--cwd <path>\s+working directory: path to root of the DS repository/);
   });
--- packages/uxpin-merge-cli/test/integration/help/summaryCommandHelp.test.ts
+++ packages/uxpin-merge-cli/test/integration/help/summaryCommandHelp.test.ts
@@ -1,27 +1,25 @@
 import { runUXPinMergeCommand } from '../../utils/command/runUXPinMergeCommand';
 
 describe('--help option for `summary` command', () => {
-  it('it prints description for summary command', async () => {
-    // when
-    const output:string = await runUXPinMergeCommand({ params: ['summary', '--help'] });
 
+  let output:string;
+
+  beforeAll(async () => {
+    output = await runUXPinMergeCommand({ params: ['summary', '--help'] });
+  });
+
+  it('it prints description for summary command', () => {
     // then
     expect(output).toContain('Show only design system summary without building it');
   });
 
-  it('it prints help for --config <path> option', async () => {
-    // when
-    const output:string = await runUXPinMergeCommand({ params: ['summary', '--help'] });
-
+  it('it prints help for --config <path> option', () => {
     // then
     expect(output)
       .toMatch(/--config <path>\s+path to a config file\. Default: `\.\/uxpin\.config\.js`/);
   });
 
-  it('it prints help for --cwd <path> option', async () => {
-    // when
-    const output:string = await runUXPinMergeCommand({ params: ['summary', '--help'] });
-
+  it('it prints help for --cwd <path> option', () => {
     // then
     expect(output).toMatch(/--cwd <path>\s+working directory: path to root of the DS repository/);
   });
