33b65a3116b8d4489baa5dffe2d05da3436b9102
Michał Stocki <michal.stocki@uxpin.com>
2018-10-16 09:15:32 +0200

    16511: Using fs-extra instead of mkdirp-promise

--- packages/uxpin-merge-cli/src/steps/building/library/createComponentsLibrary.ts
+++ packages/uxpin-merge-cli/src/steps/building/library/createComponentsLibrary.ts
@@ -1,12 +1,12 @@
 import { writeFile } from 'fs';
-import { mkdir } from '../../../utils/fs/mkdir';
+import { ensureDir } from 'fs-extra';
 import { ComponentDefinition } from '../../serialization/component/ComponentDefinition';
 import { LIBRARY_INPUT_PATH, TEMP_DIR_PATH } from '../config/getConfig';
 import { getFileString } from './getFileString';
 
 export function createComponentsLibrary(componentInfos:ComponentDefinition[], wrapperPath?:string):Promise<string> {
   return new Promise((resolve, reject) => {
-    mkdir(TEMP_DIR_PATH)
+    ensureDir(TEMP_DIR_PATH)
       .then(() => writeFile(LIBRARY_INPUT_PATH, getFileString(componentInfos, wrapperPath), 'utf8', (error) => {
         if (error) {
           return reject(error.message);
