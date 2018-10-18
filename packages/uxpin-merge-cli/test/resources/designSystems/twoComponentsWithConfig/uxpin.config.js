35855181df0ba8e5b09098d0ca6958a5a2688001
Michał Stocki <michal.stocki@uxpin.com>
2018-10-18 17:24:18 +0200

    16910: Added basic example DS for testing purpose

---
+++ packages/uxpin-merge-cli/test/resources/designSystems/twoComponentsWithConfig/uxpin.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: 'General',
+        include: [
+          'src/components/Avatar/Avatar.tsx',
+        ]
+      },
+      {
+        name: 'Forms',
+        include: 'src/components/Button/Button.tsx'
+      },
+    ]
+  }
+};
