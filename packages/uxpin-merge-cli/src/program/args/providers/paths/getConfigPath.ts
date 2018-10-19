3d8246eabcbf0b2a2c55802d832355c4c20c16ad
Michał Stocki <michal.stocki@uxpin.com>
2018-10-19 12:34:25 +0200

    16910: Support `--cwd` as a relative path

--- packages/uxpin-merge-cli/src/program/args/providers/paths/getConfigPath.ts
+++ packages/uxpin-merge-cli/src/program/args/providers/paths/getConfigPath.ts
@@ -1,9 +1,6 @@
-import { isAbsolute, join } from 'path';
+import { resolve } from 'path';
 import { ProgramArgs } from '../../ProgramArgs';
 
 export function getConfigPath({ cwd, config }:ProgramArgs):string {
-  if (isAbsolute(config)) {
-    return config;
-  }
-  return join(cwd, config);
+  return resolve(cwd, config);
 }
