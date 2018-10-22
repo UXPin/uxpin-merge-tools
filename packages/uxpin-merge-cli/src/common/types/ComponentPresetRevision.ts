72706473cf95b8e0eeec3fc78ea4c3ebd5870653
Michał Stocki <michal.stocki@uxpin.com>
2018-10-22 11:14:22 +0200

    16910: Minor code-style fixes

--- packages/uxpin-merge-cli/src/common/types/ComponentPresetRevision.ts
+++ packages/uxpin-merge-cli/src/common/types/ComponentPresetRevision.ts
@@ -1,13 +1,7 @@
 import { ComponentPreset } from '../../steps/serialization/component/presets/ComponentPreset';
 
 export interface ComponentPresetRevision extends ComponentPreset {
-  /**
-   * Constructed with `${revisionId}_${componentId}`
-   */
   componentRevisionId:string;
-  /**
-   * Automatically generated unique string
-   */
   presetId:string;
   sortIndex:number;
 }
--- packages/uxpin-merge-cli/src/common/types/PageData.ts
+++ packages/uxpin-merge-cli/src/common/types/PageData.ts
@@ -1,17 +1,17 @@
 import { CodeSyncMetadata } from './CodeSyncMetadata';
 
 export interface PageData {
-  page:PageContent;
-  last_update:string | number;
-  component_version:any;
-  is_component?:string;
-  components_master_ids?:number[];
-  components_versions?:number[];
-  components_versions_map?:number[];
   code_sync?:Partial<CodeSyncMetadata> | { presets:never[], components:never[] };
+  component_version:any;
   components?:{ [componentId:number]:PageContent };
   components_ids?:string[];
+  components_master_ids?:number[];
   components_nested_versions?:{ [componentId:number]:string };
+  components_versions?:number[];
+  components_versions_map?:number[];
+  is_component?:string;
+  last_update:string | number;
+  page:PageContent;
 }
 
 interface PageContent {
