4d01bc0063ba73c1a36f911dcffc6f4a6ed4f8e2
Michał Stocki <michal.stocki@uxpin.com>
2018-10-24 09:17:42 +0200

    16546: Help: Avoid running command in each test

--- packages/uxpin-merge-cli/test/integration/help/serverCommandHelp.test.ts
+++ packages/uxpin-merge-cli/test/integration/help/serverCommandHelp.test.ts
@@ -6,40 +6,36 @@ const CURRENT_TIMEOUT:number = 20000;
 setTimeoutBeforeAll(CURRENT_TIMEOUT);
 
 describe('--help option for `server` command', () => {
-  it('it prints help for --webpack-config <path> option', () => {
+
+  let output:string;
+
+  beforeAll(async () => {
     // when
-    return runUXPinMergeCommand({ params: ['server', '--help'] }).then((output) => {
-      // then
-      expect(output).toMatch(
-        /--webpack-config <path>\s+path to a custom webpack config, relative to the current working directory/,
-      );
-    });
+    output = await runUXPinMergeCommand({ params: ['--help'] });
+  });
+
+  it('it prints help for --webpack-config <path> option', () => {
+    // then
+    expect(output).toMatch(
+      /--webpack-config <path>\s+path to a custom webpack config, relative to the current working directory/,
+    );
   });
 
   it('it prints help for --wrapper <path> option', () => {
-    // when
-    return runUXPinMergeCommand({ params: ['server', '--help'] }).then((output) => {
-      // then
-      expect(output)
-        .toMatch(/--wrapper <path>\s+path to a custom component wrapper, relative to the current working directory/);
-    });
+    // then
+    expect(output)
+      .toMatch(/--wrapper <path>\s+path to a custom component wrapper, relative to the current working directory/);
   });
 
   it('it prints help for --cwd <path> option', () => {
-    // when
-    return runUXPinMergeCommand({ params: ['server', '--help'] }).then((output) => {
-      // then
-      expect(output)
-        .toMatch(/--cwd <path>\s+working directory: path to root of the DS repository/);
-    });
+    // then
+    expect(output)
+      .toMatch(/--cwd <path>\s+working directory: path to root of the DS repository/);
   });
 
   it('it prints help for --port <number> option', () => {
-    // when
-    return runUXPinMergeCommand({ params: ['server', '--help'] }).then((output) => {
-      // then
-      expect(output)
-        .toMatch(/--port <number>\s+port number/);
-    });
+    // then
+    expect(output)
+      .toMatch(/--port <number>\s+port number/);
   });
 });
