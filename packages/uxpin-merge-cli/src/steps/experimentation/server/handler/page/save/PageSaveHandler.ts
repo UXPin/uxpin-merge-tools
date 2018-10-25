40c464769d39a464dbf9a9e8dc97ba6d080b8bba
Michał Stocki <michal.stocki@uxpin.com>
2018-10-25 22:08:25 +0200

    16546: Refactored PageSaveHandler

--- packages/uxpin-merge-cli/src/steps/experimentation/server/handler/page/save/PageSaveHandler.ts
+++ packages/uxpin-merge-cli/src/steps/experimentation/server/handler/page/save/PageSaveHandler.ts
@@ -1,15 +1,12 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import { OK } from 'http-status-codes';
-import { isArray, reduce } from 'lodash';
-import { parse } from 'querystring';
-import { PageContent } from '../../../../../../common/types/PageData';
-import { DeletedElements, PageIncrementalUpdate } from '../../../../../../common/types/PageIncrementalUpdate';
-import { getPageContent } from '../../../common/page/content/getPageContent';
-import { writePageContent } from '../../../common/page/content/writePageContent';
+import { PageIncrementalUpdate } from '../../../../../../common/types/PageIncrementalUpdate';
+import { collectUrlEncodedFormData } from '../../../common/form/collectUrlEncodedFormData';
 import { getAccessControlHeaders } from '../../../headers/getAccessControlHeaders';
 import { ExperimentationServerContext } from '../../../startExperimentationServer';
 import { handleImplementationError } from '../../error/handleImplementationError';
 import { RequestHandler } from '../../RequestHandler';
+import { updatePage } from './updatePage';
 
 export const PAGE_FILE_NAME:string = 'page.json';
 
@@ -24,7 +21,7 @@ export class PageSaveHandler implements RequestHandler {
   }
 
   private async handleSaveRequest(request:IncomingMessage, response:ServerResponse):Promise<void> {
-    const formData:ParsedFormData = await this.getFormData(request);
+    const formData:ParsedFormData = await collectUrlEncodedFormData(request);
     const requestPayload:PageIncrementalUpdate = JSON.parse(formData.json);
     await this.updatePage(requestPayload);
     response.writeHead(OK, {
@@ -35,61 +32,11 @@ export class PageSaveHandler implements RequestHandler {
     response.end();
   }
 
-  private getFormData(request:IncomingMessage):Promise<ParsedFormData> {
-    return new Promise((resolve) => {
-      let body:string = '';
-      request.on('data', (chunk) => {
-        body += chunk.toString();
-      });
-      request.on('end', () => {
-        resolve(parse(body));
-      });
-    });
+  private updatePage(changes:PageIncrementalUpdate):Promise<void> {
+    return updatePage(this.context.uxpinDirPath, changes);
   }
-
-  private async updatePage(changes:PageIncrementalUpdate):Promise<void> {
-    const oldPage:PageContent = await getPageContent(this.context.uxpinDirPath);
-    const remainingElements:PageContent = removeElements(oldPage, changes.deleted_elements);
-    const newPage:PageContent = {
-      ...remainingElements,
-      ...reduce(changes.changed_elements, (newContent, element, elementId) => {
-        newContent[elementId] = {
-          ...element,
-          props: {
-            ...(oldPage[elementId] || { props: {} }).props,
-            ...element.props,
-          },
-        };
-        return newContent;
-      }, {} as PageContent),
-    };
-    await writePageContent(this.context.uxpinDirPath, newPage);
-  }
-
 }
 
 export interface ParsedFormData {
   json:string;
-}
-
-function removeElements(pageContent:PageContent, deleted:DeletedElements):PageContent {
-  const elementIds:string[] = getElementIds(deleted);
-  return reduce(pageContent, (newContent, element, elementId) => {
-    if (!elementIds.includes(elementId)) {
-      newContent[elementId] = element;
-    }
-    return newContent;
-  }, {} as PageContent);
-}
-
-function getElementIds(deleted:DeletedElements):string[] {
-  if (isArray(deleted)) {
-    return [];
-  }
-  return reduce(deleted, (ids, isDeleted, elementId) => {
-    if (isDeleted) {
-      ids.push(elementId);
-    }
-    return ids;
-  }, [] as string[]);
 }
---
+++ packages/uxpin-merge-cli/src/steps/experimentation/server/handler/page/save/updatePage.ts
@@ -0,0 +1,51 @@
+import { isArray, reduce } from 'lodash';
+import { PageContent } from '../../../../../../common/types/PageData';
+import { DeletedElements, PageIncrementalUpdate } from '../../../../../../common/types/PageIncrementalUpdate';
+import { getPageContent } from '../../../common/page/content/getPageContent';
+import { writePageContent } from '../../../common/page/content/writePageContent';
+
+export async function updatePage(uxpinDirPath:string, changes:PageIncrementalUpdate):Promise<void> {
+  const oldPage:PageContent = await getPageContent(uxpinDirPath);
+  const newPage:PageContent = {
+    ...removeElements(oldPage, changes.deleted_elements),
+    ...updateElements(oldPage, changes.changed_elements),
+  };
+  await writePageContent(uxpinDirPath, newPage);
+}
+
+function updateElements(oldPage:PageContent, changedElements:Partial<PageContent>):PageContent {
+  return reduce(changedElements, (newContent, elementChanges, elementId) => {
+    const oldElement:{ props:{} } = oldPage[elementId] || { props: {} };
+    newContent[elementId] = {
+      ...oldElement,
+      ...elementChanges,
+      props: {
+        ...oldElement.props,
+        ...elementChanges.props,
+      },
+    };
+    return newContent;
+  }, {} as PageContent);
+}
+
+function removeElements(pageContent:PageContent, deleted:DeletedElements):PageContent {
+  const elementIds:string[] = getElementIds(deleted);
+  return reduce(pageContent, (newContent, element, elementId) => {
+    if (!elementIds.includes(elementId)) {
+      newContent[elementId] = element;
+    }
+    return newContent;
+  }, {} as PageContent);
+}
+
+function getElementIds(deleted:DeletedElements):string[] {
+  if (isArray(deleted)) {
+    return [];
+  }
+  return reduce(deleted, (ids, isDeleted, elementId) => {
+    if (isDeleted) {
+      ids.push(elementId);
+    }
+    return ids;
+  }, [] as string[]);
+}
