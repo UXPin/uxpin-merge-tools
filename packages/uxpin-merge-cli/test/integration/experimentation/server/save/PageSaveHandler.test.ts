2140e14d39d8df31b1397f047c534d21df07aa4d
Michał Stocki <michal.stocki@uxpin.com>
2018-10-31 11:30:15 +0100

    16546: Added one more element to tests of PageSaveHandler

--- packages/uxpin-merge-cli/test/integration/experimentation/server/save/PageSaveHandler.test.ts
+++ packages/uxpin-merge-cli/test/integration/experimentation/server/save/PageSaveHandler.test.ts
@@ -10,7 +10,7 @@ import { PAGE_FILE_NAME } from '../../../../../src/steps/experimentation/server/
 import { setTimeoutBeforeAll } from '../../../../utils/command/setTimeoutBeforeAll';
 import { setupExperimentationServerTest } from '../../../../utils/experimentation/setupExperimentationServerTest';
 import { addSecondElementRequestPayload } from './fixtures/addSecondElementRequestPayload';
-import { createFirstElementRequestPayload } from './fixtures/createFirstElementRequestPayload';
+import { createFirstElementsRequestPayload } from './fixtures/createFirstElementsRequestPayload';
 import { deleteElementRequestPayload } from './fixtures/deleteElementRequestPayload';
 import { updateElementRequestPayload } from './fixtures/updateElementRequestPayload';
 
@@ -20,12 +20,12 @@ setTimeoutBeforeAll(CURRENT_TIMEOUT);
 describe('Experimentation server – handling save page request', () => {
   const { request, getWorkingDir } = setupExperimentationServerTest();
 
-  describe('saving the first element', () => {
+  describe('saving the first elements', () => {
     let response:Response;
 
     beforeAll(async () => {
       // when
-      response = await performSaveRequestWith(createFirstElementRequestPayload);
+      response = await performSaveRequestWith(createFirstElementsRequestPayload);
     });
 
     it('responds with OK status code and correct headers', async () => {
@@ -45,12 +45,13 @@ describe('Experimentation server – handling save page request', () => {
     it('correctly saves first element', async () => {
       // given
       const expectedPageContent:PageContent = {
-        '46a48bee71ce4c20bbc1d1ee97b3891f':
-          createFirstElementRequestPayload.changed_elements['46a48bee71ce4c20bbc1d1ee97b3891f'],
+        '46a48bee': createFirstElementsRequestPayload.changed_elements['46a48bee'],
+        '83ty393l': createFirstElementsRequestPayload.changed_elements['83ty393l'],
         canvas: {
           props: {
             storedElements: [
-              '46a48bee71ce4c20bbc1d1ee97b3891f',
+              '46a48bee',
+              '83ty393l',
             ],
           }, type: 'Canvas', v: '2.0',
         },
@@ -69,19 +70,21 @@ describe('Experimentation server – handling save page request', () => {
       it('correctly updates existing elements', async () => {
         // given
         const expectedPageContent:PageContent = {
-          '46a48bee71ce4c20bbc1d1ee97b3891f': {
+          '46a48bee': {
             props: {
-              ...createFirstElementRequestPayload.changed_elements['46a48bee71ce4c20bbc1d1ee97b3891f'].props,
+              ...createFirstElementsRequestPayload.changed_elements['46a48bee'].props,
               height: 163,
               width: 347,
             },
             type: 'Box',
             v: '2.0',
           },
+          '83ty393l': createFirstElementsRequestPayload.changed_elements['83ty393l'],
           canvas: {
             props: {
               storedElements: [
-                '46a48bee71ce4c20bbc1d1ee97b3891f',
+                '46a48bee',
+                '83ty393l',
               ],
             }, type: 'Canvas', v: '2.0',
           },
@@ -100,25 +103,23 @@ describe('Experimentation server – handling save page request', () => {
         it('correctly saves the new element', async () => {
           // given
           const expectedPageContent:PageContent = {
-            '46a48bee71ce4c20bbc1d1ee97b3891f': {
+            '46a48bee': {
               props: {
-                ...createFirstElementRequestPayload.changed_elements['46a48bee71ce4c20bbc1d1ee97b3891f'].props,
+                ...createFirstElementsRequestPayload.changed_elements['46a48bee'].props,
                 height: 163,
                 width: 347,
               },
               type: 'Box',
               v: '2.0',
             },
-            b5b8401708bf42df9e2b5ef5cca419bf: {
-              props: addSecondElementRequestPayload.changed_elements.b5b8401708bf42df9e2b5ef5cca419bf.props,
-              type: 'Circle',
-              v: '2.0',
-            },
+            '83ty393l': createFirstElementsRequestPayload.changed_elements['83ty393l'],
+            b5b84017: addSecondElementRequestPayload.changed_elements.b5b84017,
             canvas: {
               props: {
                 storedElements: [
-                  '46a48bee71ce4c20bbc1d1ee97b3891f',
-                  'b5b8401708bf42df9e2b5ef5cca419bf',
+                  '46a48bee',
+                  '83ty393l',
+                  'b5b84017',
                 ],
               }, type: 'Canvas', v: '2.0',
             },
@@ -137,15 +138,13 @@ describe('Experimentation server – handling save page request', () => {
           it('correctly removes elements', async () => {
             // given
             const expectedPageContent:PageContent = {
-              b5b8401708bf42df9e2b5ef5cca419bf: {
-                props: addSecondElementRequestPayload.changed_elements.b5b8401708bf42df9e2b5ef5cca419bf.props,
-                type: 'Circle',
-                v: '2.0',
-              },
+              '83ty393l': createFirstElementsRequestPayload.changed_elements['83ty393l'],
+              b5b84017: addSecondElementRequestPayload.changed_elements.b5b84017,
               canvas: {
                 props: {
                   storedElements: [
-                    'b5b8401708bf42df9e2b5ef5cca419bf',
+                    '83ty393l',
+                    'b5b84017',
                   ],
                 }, type: 'Canvas', v: '2.0',
               },
--- packages/uxpin-merge-cli/test/integration/experimentation/server/save/fixtures/deleteElementRequestPayload.ts
+++ packages/uxpin-merge-cli/test/integration/experimentation/server/save/fixtures/deleteElementRequestPayload.ts
@@ -4,14 +4,14 @@ export const deleteElementRequestPayload:PageIncrementalUpdate = {
   can_break_cohesion: false,
   changed_elements: {
     canvas: {
-      props: { storedElements: ['b5b8401708bf42df9e2b5ef5cca419bf'] },
+      props: { storedElements: ['83ty393l', 'b5b84017'] },
       type: 'Canvas',
       v: '2.0',
     },
   },
   comet_thread_id: '8997684629855957696973',
   components_used: {},
-  deleted_elements: { '46a48bee71ce4c20bbc1d1ee97b3891f': true },
+  deleted_elements: { '46a48bee': true },
   design_system_used: {},
   id_page: '95454384',
   id_project: '7696973',
