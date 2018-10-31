ab6b99989a06f16ca93a8920af6498d4b53e7f50
Michał Stocki <michal.stocki@uxpin.com>
2018-10-31 10:53:36 +0100

    16546: Reject promise on request error

--- packages/uxpin-merge-cli/src/steps/experimentation/server/common/form/collectUrlEncodedFormData.ts
+++ packages/uxpin-merge-cli/src/steps/experimentation/server/common/form/collectUrlEncodedFormData.ts
@@ -2,13 +2,16 @@ import { IncomingMessage } from 'http';
 import { parse } from 'querystring';
 
 export function collectUrlEncodedFormData(request:IncomingMessage):Promise<any> {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     let body:string = '';
     request.on('data', (chunk) => {
       body += chunk.toString();
     });
     request.on('end', () => {
       resolve(parse(body));
+    });
+    request.on('error', (error) => {
+    	reject(error);
     });
   });
 }
