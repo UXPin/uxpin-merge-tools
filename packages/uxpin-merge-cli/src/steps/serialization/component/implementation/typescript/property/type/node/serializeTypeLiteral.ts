3680567aef1e8538f803d18c780fb3f48812a8dc
Michał Stocki <michal.stocki@uxpin.com>
2018-12-11 21:55:50 +0100

    17265: Support object type literals with property as shape types

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/type/node/serializeTypeLiteral.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/type/node/serializeTypeLiteral.ts
@@ -3,12 +3,12 @@ import * as ts from 'typescript';
 import { PropertyType } from '../../../../ComponentPropertyDefinition';
 
 export function serializeTypeLiteral(typeNode:ts.TypeLiteralNode):PropertyType {
-  if (containsIndexSignature(typeNode)) {
+  if (isObjectLike(typeNode)) {
     return { name: 'shape', structure: {} };
   }
   return { name: 'unsupported', structure: { raw: typeNode.getText() } };
 }
 
-function containsIndexSignature(typeNode:ts.TypeLiteralNode):boolean {
-  return !!typeNode.members.find((m) => ts.isIndexSignatureDeclaration(m));
+function isObjectLike(typeNode:ts.TypeLiteralNode):boolean {
+  return !!typeNode.members.find((m) => ts.isIndexSignatureDeclaration(m) || ts.isPropertySignature(m));
 }
