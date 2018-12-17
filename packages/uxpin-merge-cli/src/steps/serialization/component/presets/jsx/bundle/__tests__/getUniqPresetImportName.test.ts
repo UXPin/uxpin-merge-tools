3524e90e73afa68f5e2a1fa76e9d6f5b1d84a479
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2018-12-17 17:51:26 +0100

    11940: Add unit tests for `getUniqPresetImportName`

---
+++ packages/uxpin-merge-cli/src/steps/serialization/component/presets/jsx/bundle/__tests__/getUniqPresetImportName.test.ts
@@ -0,0 +1,41 @@
+import { getUniqPresetImportName } from '../getUniqPresetImportName';
+
+describe('getUniqPresetImportName', () => {
+  it('should return valid import name', () => {
+    // given
+    const path:string = './src/components/Button/presets/0-default.jsx';
+    const expectedImportName:string = 'Preset445bc29ee1845bac85fade5cad39109d';
+
+    // when
+    const result:string = getUniqPresetImportName(path);
+
+    // then
+    expect(result).toEqual(expectedImportName);
+  });
+
+  it('should return the same import name for the same input', () => {
+    // given
+    const path1:string = './src/components/Button/presets/1-basic.jsx';
+    const path2:string = './src/components/Button/presets/1-basic.jsx';
+
+    // when
+    const result1:string = getUniqPresetImportName(path1);
+    const result2:string = getUniqPresetImportName(path2);
+
+    // then
+    expect(result1).toEqual(result2);
+  });
+
+  it('should return different import name for different input', () => {
+    // given
+    const path1:string = './src/components/Button/presets/1-basic.jsx';
+    const path2:string = './src/components/Card/presets/1-basic.jsx';
+
+    // when
+    const result1:string = getUniqPresetImportName(path1);
+    const result2:string = getUniqPresetImportName(path2);
+
+    // then
+    expect(result1).not.toEqual(result2);
+  });
+});
