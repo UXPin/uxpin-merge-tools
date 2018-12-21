d64af50f239b49c56559a85d108320208072a724
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2018-12-21 11:32:09 +0100

    18115: Added async child_process.exec

---
+++ packages/uxpin-merge-cli/src/utils/child_process/__tests__/execAsync.test.ts
@@ -0,0 +1,27 @@
+import { execAsync } from "../execAsync";
+
+describe('execAsync', () => {
+  it('should execute command', async () => {
+    // having
+    const command:string = 'pwd';
+
+    // when
+    const result:string = await execAsync(command);
+
+    // then
+    expect(result).toContain('uxpin-code-tools/packages/uxpin-merge-cli');
+  });
+
+  it('should reject on error', async () => {
+    // having
+    const command:string = 'some_unknown_command';
+
+    try {
+      // when
+      await execAsync(command);
+    } catch (error) {
+      // then
+      expect(error.toString()).toContain('command not found');
+    }
+  });
+});
