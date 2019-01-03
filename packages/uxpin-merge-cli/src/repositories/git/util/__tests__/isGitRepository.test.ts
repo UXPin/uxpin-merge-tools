3793da28d9af6ebb6355cc97ecc4c93d14b30215
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-01-03 11:36:19 +0100

    18116: Get proper repository adapter

---
+++ packages/uxpin-merge-cli/src/repositories/git/util/__tests__/isGitRepository.test.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { isGitRepository } from '../isGitRepository';
+
+describe('itGitRepository', () => {
+  it('should return true if directory belongs to a valid git repository', async () => {
+    // given
+    const cwd:string = resolve(__dirname, '../../../../../test/resources/repos/git-repo');
+
+    // when
+    const isGit:boolean = await isGitRepository(cwd);
+
+    // then
+    expect(isGit).toBe(true);
+  });
+
+  it('should return false if directory is not a valid git repository', async () => {
+    // given
+    const cwd:string = resolve(__dirname, '../../../../../../../../');
+
+    // when
+    const isGit:boolean = await isGitRepository(cwd);
+
+    // then
+    expect(isGit).toBe(false);
+  });
+});
