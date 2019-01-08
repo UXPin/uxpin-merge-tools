7cb28cceb100012b6719d2369a56a09df9ef8a4f
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2019-01-08 13:07:44 +0100

    11940: Skip old arui and nordnet design system integration tests

--- packages/uxpin-merge-cli/test/integration/dump/arui-feather-dump.test.ts
+++ packages/uxpin-merge-cli/test/integration/dump/arui-feather-dump.test.ts
@@ -5,7 +5,7 @@ import { setTimeoutBeforeAll } from '../../utils/command/setTimeoutBeforeAll';
 const CURRENT_TIMEOUT:number = 30000;
 setTimeoutBeforeAll(CURRENT_TIMEOUT);
 
-describe('The dump command', () => {
+xdescribe('The dump command', () => {
   describe('run for the arui-feather repository', () => {
     it('prints the JSON describing the full repository', () => {
       // when
