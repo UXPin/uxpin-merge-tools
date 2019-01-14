1ccfcfe9b0939a38a18a8e828c85a144f909d067
Michał Stocki <michal.stocki@uxpin.com>
2019-01-14 23:35:02 +0100

    17265: Selection of Polaris components to be serialized in tests

--- packages/uxpin-merge-cli/test/resources/configs/polaris-uxpin.config.js
+++ packages/uxpin-merge-cli/test/resources/configs/polaris-uxpin.config.js
@@ -14,21 +14,21 @@ module.exports = {
           // 'src/components/ButtonGroup/ButtonGroup.tsx',
           // 'src/components/CalloutCard/CalloutCard.tsx',
           // 'src/components/Caption/Caption.tsx',
-          'src/components/Card/Card.tsx', // export default, class, pure component
+          // 'src/components/Card/Card.tsx',
           // 'src/components/Checkbox/Checkbox.tsx',
           'src/components/Choice/Choice.tsx', // export default, functional, non-children ReactNode property
           // 'src/components/ChoiceList/ChoiceList.tsx',
           'src/components/Collapsible/Collapsible.tsx', // export default, class, classic, stateful component
           // 'src/components/ColorPicker/ColorPicker.tsx',
           // 'src/components/Connected/Connected.tsx',
-          // 'src/components/DatePicker/DatePicker.tsx',
+          'src/components/DatePicker/DatePicker.tsx', // export default, class; Date, numeric enum, complex union in props, callback with value
           'src/components/DescriptionList/DescriptionList.tsx', // export default, functional, ReactNode value in data structure
           // 'src/components/DisplayText/DisplayText.tsx',
           // 'src/components/EmptyState/EmptyState.tsx',
           // 'src/components/FooterHelp/FooterHelp.tsx',
           // 'src/components/FormLayout/FormLayout.tsx',
           // 'src/components/Heading/Heading.tsx',
-          // 'src/components/Icon/Icon.tsx',
+          'src/components/Icon/Icon.tsx', // export default, functional, union property with shape and keyof typeof
           // 'src/components/Image/Image.tsx',
           // 'src/components/KeyboardKey/KeyboardKey.tsx',
           // 'src/components/Label/Label.tsx',
@@ -36,14 +36,14 @@ module.exports = {
           // 'src/components/Layout/Layout.tsx',
           // 'src/components/Link/Link.tsx',
           // 'src/components/List/List.tsx',
-          // 'src/components/Page/Page.tsx',
-          // 'src/components/PageActions/PageActions.tsx',
+          'src/components/Page/Page.tsx', // props extending imported props, index types, required props
+          'src/components/PageActions/PageActions.tsx', // intersection type in props
           // 'src/components/Pagination/Pagination.tsx',
-          // 'src/components/Popover/Popover.tsx',
-          // 'src/components/PositionedOverlay/PositionedOverlay.tsx',
+          'src/components/Popover/Popover.tsx', // react element property,
+          'src/components/PositionedOverlay/PositionedOverlay.tsx', // html element property,
           // 'src/components/RadioButton/RadioButton.tsx',
           // 'src/components/ResourceList/ResourceList.tsx',
-          // 'src/components/Scrollable/Scrollable.tsx',
+          'src/components/Scrollable/Scrollable.tsx', // props extending React.HTMLProps
           // 'src/components/Select/Select.tsx',
           // 'src/components/SettingAction/SettingAction.tsx',
           // 'src/components/SettingToggle/SettingToggle.tsx',
@@ -60,7 +60,7 @@ module.exports = {
           // 'src/components/TextStyle/TextStyle.tsx',
           // 'src/components/Thumbnail/Thumbnail.tsx',
           // 'src/components/Tooltip/Tooltip.tsx',
-          // 'src/components/UnstyledLink/UnstyledLink.tsx',
+          'src/components/UnstyledLink/UnstyledLink.tsx', // index signature in props, extends React.HTMLProps
           // 'src/components/VisuallyHidden/VisuallyHidden.tsx',
         ]
       }
