4931e4058086b9d6673b327cc3bbabd90f567739
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2019-01-20 21:47:23 +0100

    11940: Refactor removing unsupported elements

--- packages/uxpin-merge-cli/src/steps/serialization/component/presets/replaceElementsWithReferencesInProps.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/presets/replaceElementsWithReferencesInProps.ts
@@ -1,35 +1,38 @@
-import { forEachRight, isArray, isObject, reduce } from 'lodash';
+import { isArray, isObject, reduce } from 'lodash';
 import { ComponentPresetElementProps } from './ComponentPreset';
 import { getPresetElementReference } from './getPresetElementReference';
 import { isJSXSerializedElement } from './isJSXSerializedElement';
 import { JSXSerializedElementProps } from './jsx/JSXSerializationResult';
 
-type PartialProps = Pick<JSXSerializedElementProps, Exclude<keyof JSXSerializedElementProps, 'uxpId'>>;
+export type PartialProps = Pick<JSXSerializedElementProps, Exclude<keyof JSXSerializedElementProps, 'uxpId'>>;
 
 export function replaceElementsWithReferencesInProps(props:PartialProps):ComponentPresetElementProps {
   return reduce<ComponentPresetElementProps, PartialProps>(props, (mappedProps, propValue, propName) => {
     if (isJSXSerializedElement(propValue)) {
       mappedProps[propName] = getPresetElementReference(propValue);
     } else {
-      mappedProps[propName] = propValue;
-      removeUnsupportedElements(propValue);
+      mappedProps[propName] = withoutUnsupportedElements(propValue);
     }
     return mappedProps;
   }, {});
 }
 
-function removeUnsupportedElements(prop:ComponentPresetElementProps | any):void {
-  if (isArray(prop) || isObject(prop)) {
-    forEachRight(prop, (subProp, key) => {
-      if (isJSXSerializedElement(subProp)) {
-        if (isArray(prop)) {
-          prop.splice(key, 1);
-        } else {
-          delete prop[key];
-        }
-      } else {
-        removeUnsupportedElements(subProp);
-      }
-    });
+function withoutUnsupportedElements(prop:ComponentPresetElementProps | any[] | any):ComponentPresetElementProps {
+  if (!isArray(prop) && !isObject(prop)) {
+    return prop;
   }
+
+  return reduce(prop, (validProp, subProp, key) => {
+    if (isJSXSerializedElement(subProp)) {
+      return validProp;
+    }
+
+    if (isArray(validProp)) {
+      validProp.push(withoutUnsupportedElements(subProp));
+    } else {
+      validProp[key] = withoutUnsupportedElements(subProp);
+    }
+
+    return validProp;
+  }, isArray(prop) ? [] : {} as ComponentPresetElementProps);
 }
