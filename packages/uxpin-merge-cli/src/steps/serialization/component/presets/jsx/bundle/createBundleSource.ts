3bb6a230b38f81f2ae331e0a3024ac54b64f68bc
jarek sawicki <jaroslaw.sawicki@uxpin.com>
2019-01-21 14:54:11 +0100

    Revert "11940: Set constant presets file name"
    This reverts commit 4bbd4c284fb614c6c42f02737994a14eebaa2f02.

--- packages/uxpin-merge-cli/src/steps/serialization/component/presets/jsx/bundle/createBundleSource.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/presets/jsx/bundle/createBundleSource.ts
@@ -1,17 +1,20 @@
 import { ensureDir } from 'fs-extra';
 import { resolve } from 'path';
+import { v4 } from 'uuid';
 import { ProgramArgs } from '../../../../../../program/args/ProgramArgs';
 import { getTempDirPath } from '../../../../../../program/args/providers/paths/getTempDirPath';
 import { writeToFile } from '../../../../../../utils/fs/writeToFile';
 import { ComponentCategoryInfo } from '../../../../../discovery/component/category/ComponentCategoryInfo';
 import { getSourceFileContentToBundle } from './getSourceFileContentToBundle';
 
-const PRESETS_FILE_NAME:string = 'presets-all.js';
-
 export async function createBundleSource(programArgs:ProgramArgs, infos:ComponentCategoryInfo[]):Promise<string> {
   const tempDirPath:string = getTempDirPath(programArgs);
   await ensureDir(tempDirPath);
-  const bundleSourcePath:string = resolve(tempDirPath, PRESETS_FILE_NAME);
+  const bundleSourcePath:string = resolve(tempDirPath, getUniqueFileName());
   await writeToFile(bundleSourcePath, getSourceFileContentToBundle(tempDirPath, infos));
   return bundleSourcePath;
+}
+
+function getUniqueFileName():string {
+  return `presets-${v4()}.js`;
 }
