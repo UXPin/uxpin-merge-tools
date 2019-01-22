f1498e9c840d6adf5714f5e8f3a10b0a2be9e4e8
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-01-22 09:49:16 +0100

    18118: Updated mineral ui push test snapshot

--- packages/uxpin-merge-cli/test/integration/push/mineral-ui-push.test.ts
+++ packages/uxpin-merge-cli/test/integration/push/mineral-ui-push.test.ts
@@ -1,13 +1,30 @@
 import { Command } from '../../../src';
+import { mineralUiServerStub } from '../../resources/stubs/mineralUi';
 import { runUXPinMergeCommand } from '../../utils/command/runUXPinMergeCommand';
 import { setTimeoutBeforeAll } from '../../utils/command/setTimeoutBeforeAll';
+import { startStubbyServer } from '../../utils/stubby/startStubbyServer';
+import { stopStubbyServer } from '../../utils/stubby/stopStubbyServer';
 
 const CURRENT_TIMEOUT:number = 75000;
+const STUBBY_PORT:number = 7448;
 setTimeoutBeforeAll(CURRENT_TIMEOUT);
 
 jest.mock('../../../src/program/utils/version/getToolVersion');
 
 describe('Pushing mineral-ui design system', () => {
+  let server:any;
+
+  beforeAll(async () => {
+    server = await startStubbyServer({
+      data: mineralUiServerStub.requests,
+      tls: STUBBY_PORT,
+    });
+  });
+
+  afterAll(async () => {
+    await stopStubbyServer(server);
+  });
+
   describe('with required user webpack config', () => {
     let consoleOutput:string;
 
@@ -16,6 +33,8 @@ describe('Pushing mineral-ui design system', () => {
         Command.PUSH,
         '--webpack-config "./webpack.config.js"',
         '--wrapper "./src/library/themes/UXPinWrapper.js"',
+        `--uxpin-domain "0.0.0.0:${STUBBY_PORT}"`,
+        `--uxpin-api-domain "0.0.0.0:${STUBBY_PORT}"`,
       ];
 
       consoleOutput = await runUXPinMergeCommand({ cwd: 'resources/repos/mineral-ui', params });
@@ -29,7 +48,13 @@ describe('Pushing mineral-ui design system', () => {
 
   describe('without required user webpack config', () => {
     it('throws an error', async () => {
-      await expect(runUXPinMergeCommand({ cwd: 'resources/repos/mineral-ui', params: [Command.PUSH] }))
+      const params:string[] = [
+        Command.PUSH,
+        `--uxpin-domain "0.0.0.0:${STUBBY_PORT}"`,
+        `--uxpin-api-domain "0.0.0.0:${STUBBY_PORT}"`,
+      ];
+
+      await expect(runUXPinMergeCommand({ cwd: 'resources/repos/mineral-ui', params }))
         .rejects.toMatch('Module parse failed: Unexpected token');
     });
   });
