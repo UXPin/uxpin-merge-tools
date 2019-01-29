499529a9a2ab9138c13859b43dd6f6ea284fd29a
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-01-29 12:38:02 +0100

    Removed unnecessary *.js and *.js.map

--- packages/uxpin-merge-cli/__mocks__/requestPromise.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const requestPromise = jest.genMockFromModule('request-promise');
-exports.default = requestPromise;
-//# sourceMappingURL=requestPromise.js.map
