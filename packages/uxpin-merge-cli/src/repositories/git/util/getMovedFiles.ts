6ed80a8d94c381a135e784fb0c36721c1283b383
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-01-29 12:38:33 +0100

    Added similarity index to git diff command

--- packages/uxpin-merge-cli/src/repositories/git/util/getMovedFiles.ts
+++ packages/uxpin-merge-cli/src/repositories/git/util/getMovedFiles.ts
@@ -4,7 +4,7 @@ import { execAsync } from '../../../utils/child_process/execAsync';
 
 export async function getMovedFiles(cwd:string, r1:string, r2:string):Promise<MovedFilePathsMap> {
   const options:ExecOptions = { cwd };
-  const diff:string = await execAsync(`git diff ${r1}...${r2} --diff-filter=R --name-status`, options);
+  const diff:string = await execAsync(`git diff ${r1}...${r2} --diff-filter=R --name-status -M99%`, options);
 
   const files:MovedFilePathsMap = diff.split('\n')
     .filter((line) => line !== '')
