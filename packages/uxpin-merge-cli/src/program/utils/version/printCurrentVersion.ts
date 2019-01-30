f89b148e6fc9fd1905b5dc1181247bfd268d47b0
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-01-30 15:30:47 +0100

    Turn off printing version in tests

--- packages/uxpin-merge-cli/src/program/utils/version/printCurrentVersion.ts
+++ packages/uxpin-merge-cli/src/program/utils/version/printCurrentVersion.ts
@@ -1,8 +1,13 @@
 import * as safe from 'colors/safe';
+import { Environment } from '../../../program/env/Environment';
 import { printLine } from '../../../utils/console/printLine';
 import { getToolVersion } from './getToolVersion';
 
 export function printCurrentVersionInfo():void {
+  if (process.env.UXPIN_ENV === Environment.TEST) {
+    return;
+  }
+
   printLine('');
   printLine(getCurrentVersionInfo());
   printLine('');
