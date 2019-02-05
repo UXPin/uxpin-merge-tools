b8a45994a2f443569d685ccf12f0a15431def52e
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-02-05 12:54:05 +0100

    Changed similarity index for git

--- packages/uxpin-merge-cli/src/steps/serialization/vcs/repositories/git/util/getMovedFiles.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/vcs/repositories/git/util/getMovedFiles.ts
@@ -2,10 +2,12 @@ import { ExecOptions } from 'child_process';
 import { execAsync } from '../../../../../../utils/child_process/execAsync';
 import { MovedFilePathsMap } from '../../../../DesignSystemSnapshot';
 
+const SIMILARITY_INDEX:number = 50;
+
 export async function getMovedFiles(cwd:string, revision1:string, revision2:string):Promise<MovedFilePathsMap> {
   const options:ExecOptions = { cwd };
   const diff:string = await execAsync(
-    `git diff ${revision1}...${revision2} --diff-filter=R --name-status -M99%`,
+    `git diff ${revision1}...${revision2} --diff-filter=R --name-status -M${SIMILARITY_INDEX}%`,
     options,
   );
 
