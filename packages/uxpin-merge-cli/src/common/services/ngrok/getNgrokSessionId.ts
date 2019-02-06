e433681d08435f070b214004c28f5fdec3da168a
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-02-06 13:14:43 +0100

    18447: Fixed epid tests

--- packages/uxpin-merge-cli/src/common/services/ngrok/getNgrokSessionId.ts
+++ packages/uxpin-merge-cli/src/common/services/ngrok/getNgrokSessionId.ts
@@ -1,4 +1,4 @@
-const NGROK_SESSION_ID_REGEXP = /https?:\/\/([a-z0-9]+)\.ngrok/;
+const NGROK_SESSION_ID_REGEXP = /https?:\/\/([a-z0-9]+)\.ngrok/i;
 
 export function getNgrokSessionId(url:string):string | null {
   const result:RegExpExecArray | null = NGROK_SESSION_ID_REGEXP.exec(url);
--- packages/uxpin-merge-cli/src/program/command/experimentation/steps/experimentationRunNgrok.ts
+++ packages/uxpin-merge-cli/src/program/command/experimentation/steps/experimentationRunNgrok.ts
@@ -1,6 +1,7 @@
 import * as ngrok from 'ngrok';
 import { ExperimentProgramArgs } from '../../../../program/args/ProgramArgs';
 import { DSMetadata } from '../../../../program/DSMeta';
+import { isTestEnv } from '../../../../program/env/isTestEnv';
 import { Store } from '../../../../program/utils/store/Store';
 import { Step, StepExecutor } from '../../Step';
 import { ExperimentationState } from '../getExperimentationCommandSteps';
@@ -9,9 +10,13 @@ export function experimentationRunNgrok(args:ExperimentProgramArgs, store:Store<
   return { exec: startNgrok(args, store), shouldRun: true };
 }
 
+const TEST_SESSION_ID:string = 'https://sessionId.ngrok.io';
+
 function startNgrok(args:ExperimentProgramArgs, store:Store<ExperimentationState>):StepExecutor {
   return async (ds:DSMetadata) => {
-    const url = await ngrok.connect(args.port);
+    const url:string = isTestEnv()
+      ? TEST_SESSION_ID
+      : await ngrok.connect(args.port);
 
     store.setState({
       ngrokUrl: url,
