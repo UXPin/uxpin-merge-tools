76d57e2dd900679179e81f616bea221d20e1f675
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-02-12 15:56:49 +0100

    18705: Removed child-process-promise package

--- packages/uxpin-merge-cli/test/utils/command/runCommand.ts
+++ packages/uxpin-merge-cli/test/utils/command/runCommand.ts
@@ -1,11 +1,6 @@
-import { exec } from 'child-process-promise';
 import { ExecOptions } from 'child_process';
+import { execAsync } from '../../../src/utils/child_process/execAsync';
 
 export function runCommand(command:string, options?:ExecOptions):Promise<string> {
-  return exec(command, options).then((result) => {
-    return result.stdout;
-  }).catch((err) => {
-    console.log(err.toString());
-    throw err.toString();
-  });
+  return execAsync(command, options);
 }
