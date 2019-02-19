78d6957698b0b0c69bd849cdf32697ff44f8f88f
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-02-19 13:23:50 +0100

    18875: Turn off chromeless remote option on CI

--- packages/uxpin-merge-cli/test/utils/e2e/chromeless/createChromeless.ts
+++ packages/uxpin-merge-cli/test/utils/e2e/chromeless/createChromeless.ts
@@ -2,7 +2,7 @@ import Chromeless from 'chromeless';
 
 export function createChromeless<T>(url:string, path:string = '/'):Chromeless<T> {
   return new Chromeless<T>({
-    remote: !!process.env.CI,
+    // remote: !!process.env.CI,
   })
     .goto(`${url}${path}`);
 }
