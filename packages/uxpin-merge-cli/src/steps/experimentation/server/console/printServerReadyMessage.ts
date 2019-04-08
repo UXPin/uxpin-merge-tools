c3c1ceaeba440982ffe5fdec21093fa46f29fc5f
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-04-08 10:09:55 +0200

    19802: Use experimentation URL instead of separator as server ready output

--- packages/uxpin-merge-cli/src/steps/experimentation/server/console/printServerReadyMessage.ts
+++ packages/uxpin-merge-cli/src/steps/experimentation/server/console/printServerReadyMessage.ts
@@ -2,14 +2,12 @@ import { getUXPinMergeBanner } from '../../../../utils/banner/getUXPinMergeBanne
 import { printLine } from '../../../../utils/console/printLine';
 
 const experimentalMode:string = 'Experimental Mode';
-const SEPARATOR:string = '-------------------';
-export const SERVER_READY_OUTPUT:RegExp = new RegExp(SEPARATOR);
+export const SERVER_READY_OUTPUT:RegExp =
+  new RegExp(/\/experiment\/([a-z0-9\-\_]+)\?(ngrok_session|port)\=([a-z0-9]+|[\d]+)/gim);
 
 export async function printServerReadyMessage(experimentationUrl:string):Promise<void> {
   printLine(getUXPinMergeBanner(experimentalMode));
   printLine('👩‍🔬 Open the following URL in your browser to enter the experimental mode:');
   printLine(experimentationUrl, { underline: true });
-  printLine('');
-  printLine(SEPARATOR);
   printLine('');
 }
