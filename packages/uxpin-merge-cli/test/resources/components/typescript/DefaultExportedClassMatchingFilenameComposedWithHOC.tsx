1d5b83d29838f8a5b16b6fe71a7dfb70b2dd02fa
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-04-17 15:10:57 +0200

    19330: Prepared integration tests fixtures

---
+++ packages/uxpin-merge-cli/test/resources/components/typescript/DefaultExportedClassMatchingFilenameComposedWithHOC.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { I18nProps, withI18n } from './hoc/withI18n';
+
+export interface Props {
+  appearance:'secondary' | 'primary' | 'link';
+  children?:string;
+}
+
+export class DefaultExportedClassMatchingFilenameComposedWithHOC
+  extends React.Component<Props & I18nProps> {
+  public render():JSX.Element {
+    const { appearance, children, i18n } = this.props;
+
+    return (
+      <div>
+        <button className={appearance}>
+          {children} {i18n}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default withI18n(DefaultExportedClassMatchingFilenameComposedWithHOC);
