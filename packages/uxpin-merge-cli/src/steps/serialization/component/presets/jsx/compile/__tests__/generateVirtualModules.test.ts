fc198881dc5fc0d695bfbf8294e64968834dbb1c
Michał Stocki <michal.stocki@uxpin.com>
2019-04-23 22:31:20 +0200

    18221: Use Virtual modules in preset serialization
    To serialize presets without importing (parsing, loading) actual
    component files.

---
+++ packages/uxpin-merge-cli/src/steps/serialization/component/presets/jsx/compile/__tests__/generateVirtualModules.test.ts
@@ -0,0 +1,46 @@
+import { ComponentInfo } from '../../../../../../discovery/component/ComponentInfo';
+import { generateVirtualModules, VirtualComponentModule } from '../generateVirtualModules';
+
+describe('generateVirtualModules', () => {
+  it('generates virtual module objects for component infos', () => {
+    const componentInfos:ComponentInfo[] = [
+      {
+        dirPath: 'src/components/Avatar',
+        documentation: { path: '' },
+        implementation: {
+          framework: 'reactjs',
+          lang: 'typescript',
+          path: 'src/components/Avatar/Avatar.tsx',
+        },
+        presets: [],
+      },
+      {
+        dirPath: 'src/packages/navigation/Menu',
+        documentation: { path: '' },
+        implementation: {
+          framework: 'reactjs',
+          lang: 'javascript',
+          path: 'src/packages/navigation/Menu/Menu.js',
+        },
+        presets: [],
+      },
+    ];
+
+    const expectedResult:VirtualComponentModule[] = [
+      {
+        moduleSource: 'module.exports = {"name":"Avatar"};',
+        path: 'src/components/Avatar/Avatar',
+      },
+      {
+        moduleSource: 'module.exports = {"name":"Menu"};',
+        path: 'src/packages/navigation/Menu/Menu',
+      },
+    ];
+
+    // when
+    const result:VirtualComponentModule[] = generateVirtualModules(componentInfos);
+
+    // then
+    expect(result).toEqual(expectedResult);
+  });
+});
