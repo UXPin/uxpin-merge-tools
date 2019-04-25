9f80bb908e872bdd786cbebcfe600e925e2b21d7
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-04-25 09:54:11 +0200

    20181: Added design system with namespaced components

---
+++ packages/uxpin-merge-cli/test/resources/designSystems/namespacedComponents/uxpin.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: 'Example Design System',
+  components: {
+    categories: [
+      {
+        name: 'Card',
+        include: [
+          'src/components/Card/**/*.tsx',
+          'src/components/Card/*.tsx',
+        ],
+      },
+      {
+        name: 'Other',
+        include: [
+          'src/components/**/*.tsx',
+          '!src/components/Card/**/*.tsx',
+          '!src/components/Card/*.tsx',
+        ],
+      },
+    ]
+  }
+};
