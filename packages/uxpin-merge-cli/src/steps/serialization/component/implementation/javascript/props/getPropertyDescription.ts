6f5c81f92360d2291880660d2862f0826c84fc2d
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-05-29 13:53:20 +0200

    20596: Fixed getting property description and snapshot

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/javascript/props/getPropertyDescription.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/javascript/props/getPropertyDescription.ts
@@ -1,17 +1,22 @@
 const LINES_DELIMETER:string = '\n';
+const GENERAL_JSDOC_TAG_PREFIX:string = '@';
 const UXPIN_JSDOC_TAG_PREFIX:string = '@uxpin';
 
 export function getPropertyDescription(desc:string = ''):string {
   const lines:string[] = getLines(desc);
 
   const descLines:string[] = [];
+  let isUXPinTagOpen:boolean = false;
   for (let i:number = 0; i < lines.length; i += 1) {
     const prevLine:string = lines[i - 1] || '';
     const line:string = lines[i];
 
+    isUXPinTagOpen = line.startsWith(UXPIN_JSDOC_TAG_PREFIX)
+      || isUXPinTagOpen && !line.startsWith(GENERAL_JSDOC_TAG_PREFIX);
+
     if (
       (line === '' && prevLine === '')
-        || line.startsWith(UXPIN_JSDOC_TAG_PREFIX)
+        || isUXPinTagOpen
     ) {
       continue;
     }
