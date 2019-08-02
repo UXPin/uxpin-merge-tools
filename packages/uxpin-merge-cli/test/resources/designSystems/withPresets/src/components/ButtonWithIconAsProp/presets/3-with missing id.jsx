1aecd4aa9e811ae1c606de5d7b0c934d495d7289
Michał Stocki <michal.stocki@uxpin.com>
2019-08-02 06:40:39 +0200

    20039: Improved fixture of preset with missing ID – test one thing

--- packages/uxpin-merge-cli/test/resources/designSystems/withPresets/src/components/ButtonWithIconAsProp/presets/3-with missing id.jsx
+++ packages/uxpin-merge-cli/test/resources/designSystems/withPresets/src/components/ButtonWithIconAsProp/presets/3-with missing id.jsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import IconStar from '../../../icons/IconStar/IconStar';
 import ButtonWithIconAsProp from '../ButtonWithIconAsProp';
 
 export default (
-  <ButtonWithIconAsProp uxpId="1" isPrimary>
-    Add to
-    <IconStar stroke="gray" strokeWidth={2} />
-    favourites
+  <ButtonWithIconAsProp isPrimary>
+    Button
   </ButtonWithIconAsProp>
 );
