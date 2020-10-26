daf979213c31b51e1b7c0c19e9e1e5281d9c50dc
Joe Patterson <jrwpatterson@gmail.com>
2020-10-26 13:37:37 +1000

    fix named consts not going to filename

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentName.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentName.ts
@@ -1,8 +1,14 @@
 import { TSSerializationContext } from '../context/getSerializationContext';
 import { getComponentFileName } from './getComponentFileName';
 import { ComponentDeclaration } from './getPropsTypeAndDefaultProps';
+import * as ts from 'typescript'
 
 export function getComponentName(context:TSSerializationContext, component:ComponentDeclaration):string {
-  return component.name && component.name.getText()
-    || getComponentFileName(context);
+  if(component.name) {
+    return component.name.getText()
+  }
+  if (ts.isArrowFunction(component) && component.parent && (component.parent as ts.ArrowFunction).name){
+    return ((component.parent as ts.ArrowFunction).name as ts.Identifier).getText()
+  }
+  return getComponentFileName(context);
 }
