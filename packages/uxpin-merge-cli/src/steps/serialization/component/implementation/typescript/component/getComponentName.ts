ed31e802e704bbcd1bf95f5705fcd5d1763857ed
Matthew Chigira <matthew@uxpin.com>
2020-10-30 17:15:05 +0900

    Appease the linter

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentName.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentName.ts
@@ -1,16 +1,17 @@
+import { ArrowFunction, Identifier, isArrowFunction } from 'typescript';
 import { TSSerializationContext } from '../context/getSerializationContext';
 import { getComponentFileName } from './getComponentFileName';
 import { ComponentDeclaration } from './getPropsTypeAndDefaultProps';
-import { ArrowFunction, isArrowFunction, Identifier } from 'typescript';
 
 export function getComponentName(context:TSSerializationContext, component:ComponentDeclaration):string {
   if (component.name) {
-    return component.name.getText()
+    return component.name.getText();
   }
   // Support named arrow functions and use the component parent's name.
   // https://github.com/UXPin/uxpin-merge-tools/issues/208
-  if (isArrowFunction(component) && component.parent && "name" in component.parent && (component.parent as ArrowFunction).name) {
-    return ((component.parent as ArrowFunction).name as Identifier).getText()
+  if (isArrowFunction(component) && component.parent &&
+      'name' in component.parent && (component.parent as ArrowFunction).name) {
+    return ((component.parent as ArrowFunction).name as Identifier).getText();
   }
   return getComponentFileName(context);
 }
