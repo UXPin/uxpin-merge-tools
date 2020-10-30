9e1a941de7b9e7d6440533d598296a7d2ffdd72b
Joe Patterson <jrwpatterson@gmail.com>
2020-10-30 18:42:34 +1000

    Fix Typescript Arrow Consts needing using the filename (#209)
    * fix named consts not going to filename
    * Apply suggestions from code review
    * Remove explicit use of ts glob import
    * Appease the linter
    
    Co-authored-by: Victor Adossi <61000371+vados-xenon@users.noreply.github.com>
    Co-authored-by: Matthew Chigira <matthew@uxpin.com>

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentName.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentName.ts
@@ -1,8 +1,17 @@
+import { ArrowFunction, Identifier, isArrowFunction } from 'typescript';
 import { TSSerializationContext } from '../context/getSerializationContext';
 import { getComponentFileName } from './getComponentFileName';
 import { ComponentDeclaration } from './getPropsTypeAndDefaultProps';
 
 export function getComponentName(context:TSSerializationContext, component:ComponentDeclaration):string {
-  return component.name && component.name.getText()
-    || getComponentFileName(context);
+  if (component.name) {
+    return component.name.getText();
+  }
+  // Support named arrow functions and use the component parent's name.
+  // https://github.com/UXPin/uxpin-merge-tools/issues/208
+  if (isArrowFunction(component) && component.parent &&
+      'name' in component.parent && (component.parent as ArrowFunction).name) {
+    return ((component.parent as ArrowFunction).name as Identifier).getText();
+  }
+  return getComponentFileName(context);
 }
