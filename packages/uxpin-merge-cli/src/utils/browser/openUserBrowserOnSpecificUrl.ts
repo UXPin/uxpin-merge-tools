f8b96ba64b7cad55e8ae7cae47eee814f2790645
Matthew Chigira <matthew@uxpin.com>
2020-12-01 17:06:44 +0900

    Replace deprecated 'opn' package with 'open', update TypeScript and utilise new feature of 'open'

--- packages/uxpin-merge-cli/src/utils/browser/openUserBrowserOnSpecificUrl.ts
+++ packages/uxpin-merge-cli/src/utils/browser/openUserBrowserOnSpecificUrl.ts
@@ -1,5 +1,8 @@
-import opn = require('opn');
+import open = require('open');
 
 export async function openUserBrowserOnSpecificUrl(url:string):Promise<void> {
-  await opn(url);
+  // Using the url option handles errors in Windows/WSL
+  // in which ^ characters are being inserted:
+  // https://github.com/UXPin/uxpin-merge-tools/issues/218
+  await open(url, { url: true });
 }
