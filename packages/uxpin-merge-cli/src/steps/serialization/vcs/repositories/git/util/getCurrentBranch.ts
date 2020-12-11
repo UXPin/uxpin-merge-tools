ff8a7fa02481df531b3f5a0458e46c4be9f248a6
Victor Adossi <vados+xenon@vadosware.io>
2020-12-11 16:41:42 +0900

    remove branch percent encoding

--- packages/uxpin-merge-cli/src/steps/serialization/vcs/repositories/git/util/getCurrentBranch.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/vcs/repositories/git/util/getCurrentBranch.ts
@@ -1,9 +1,8 @@
 import { DEFAULT_BRANCH_NAME } from '../../../../../../common/constants';
-import { encodeBranchName } from '../../../../../../common/services/UXPin/params/encodeBranchName';
 
 export async function getCurrentBranch(cwd:string, override?:string):Promise<string> {
   // Use branch override if one is provided
-  const branch:string|undefined = encodeBranchName(override || DEFAULT_BRANCH_NAME);
+  const branch:string|undefined = override;
 
   // If branch is not already provided, source from shell
   if (!branch) {
