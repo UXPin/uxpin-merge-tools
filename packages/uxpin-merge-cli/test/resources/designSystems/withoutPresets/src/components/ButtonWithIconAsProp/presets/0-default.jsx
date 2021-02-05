5a0643a644152d9d9659ca0503c090983158810f
Robert Hilscher <r.hilscher@gmail.com>
2021-02-05 13:53:53 +0100

    add tests for js components

---
+++ packages/uxpin-merge-cli/test/resources/designSystems/withoutPresets/src/components/ButtonWithIconAsProp/presets/0-default.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ButtonWithIconAsProp from '../ButtonWithIconAsProp';
+
+export default (
+  <ButtonWithIconAsProp
+    uxpId="button-with-icon-as-prop-1"
+    iconStart=""
+    isPrimary={false}
+  >Put ButtonWithIconAsProp contents here</ButtonWithIconAsProp>
+);
