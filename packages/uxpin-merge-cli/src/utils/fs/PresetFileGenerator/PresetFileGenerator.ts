c6edc14bbe183ede45a70e62e117d1672159b1c5
Robert Hilscher <r.hilscher@gmail.com>
2021-02-05 14:05:32 +0100

    code formatting

--- packages/uxpin-merge-cli/src/utils/fs/PresetFileGenerator/PresetFileGenerator.ts
+++ packages/uxpin-merge-cli/src/utils/fs/PresetFileGenerator/PresetFileGenerator.ts
@@ -33,102 +33,102 @@ export class PresetFileGenerator {
   private presetFilePath:string;
 
   constructor(componentPath:string) {
-  	this.componentPath = componentPath;
-  	this.componentDirectory = dirname(componentPath);
-	  this.presetDirectory = `${this.componentDirectory}/presets`;
-	  this.presetFilePath = `${this.presetDirectory}/${PRESET_FILE_NAME}`;
+    this.componentPath = componentPath;
+    this.componentDirectory = dirname(componentPath);
+    this.presetDirectory = `${this.componentDirectory}/presets`;
+    this.presetFilePath = `${this.presetDirectory}/${PRESET_FILE_NAME}`;
   }
 
   public async init():Promise<void> {
-	  if (await pathExists(resolve(__dirname, this.componentPath))) {
-		  throw new Error(`Component ${this.componentPath} does not exists`);
-	  }
+    if (await pathExists(resolve(__dirname, this.componentPath))) {
+      throw new Error(`Component ${this.componentPath} does not exists`);
+    }
 
-	  const implementationInfo:ComponentImplementationInfo | null = getImplementationInfo(this.componentPath);
+    const implementationInfo:ComponentImplementationInfo | null = getImplementationInfo(this.componentPath);
 
-	  if (!implementationInfo) {
-		  throw Error(`Invalid component path - ${this.componentPath}`);
-	  }
+    if (!implementationInfo) {
+      throw Error(`Invalid component path - ${this.componentPath}`);
+    }
 
-	  const { result: metadata, warnings: metadataWarnings } = await getComponentMetadata(implementationInfo);
+    const { result: metadata, warnings: metadataWarnings } = await getComponentMetadata(implementationInfo);
 
-	  if (metadataWarnings.length && !metadata.properties.length) {
-	  	throw new Error(metadataWarnings[0].message);
-	  }
+    if (metadataWarnings.length && !metadata.properties.length) {
+      throw new Error(metadataWarnings[0].message);
+    }
 
-	  this.componentMetadata = metadata;
-	  this.componentName = this.componentMetadata.name;
-	  this.setPropertiesValues();
+    this.componentMetadata = metadata;
+    this.componentName = this.componentMetadata.name;
+    this.setPropertiesValues();
   }
 
   public async createPresetFile():Promise<void> {
-	  if (!await pathExists(this.presetDirectory)) {
-		  await mkdir(this.presetDirectory);
-	  }
+    if (!await pathExists(this.presetDirectory)) {
+      await mkdir(this.presetDirectory);
+    }
 
-	  if (await pathExists(this.presetFilePath)) {
-	  	printWarning(`File ${this.presetFilePath} exists`);
-	  	return;
-	  }
+    if (await pathExists(this.presetFilePath)) {
+      printWarning(`File ${this.presetFilePath} exists`);
+      return;
+    }
 
-	  const componentFileContent:string = this.generateComponentFile();
-	  await writeToFile(this.presetFilePath, componentFileContent);
-	  printLine(`File ${this.presetFilePath} created successfully`, { color: PrintColor.GREEN });
+    const componentFileContent:string = this.generateComponentFile();
+    await writeToFile(this.presetFilePath, componentFileContent);
+    printLine(`File ${this.presetFilePath} created successfully`, { color: PrintColor.GREEN });
   }
 
   public generateComponentFile():string {
-  	return [
-  		this.generateImports(),
-		  '',
-		  'export default (',
-		  `${INDENTATION_CHAR}<${this.componentName}`,
-		  `${this.generatePropertiesList()}`,
-		  `${this.generateComponentEnd()}`,
-		  ');',
-	  ].join('\n');
+    return [
+      this.generateImports(),
+      '',
+      'export default (',
+      `${INDENTATION_CHAR}<${this.componentName}`,
+      `${this.generatePropertiesList()}`,
+      `${this.generateComponentEnd()}`,
+      ');',
+    ].join('\n');
   }
 
   private setPropertiesValues():void {
-  	this.componentProperties = this.componentMetadata.properties.reduce((result:Property[], prop) => {
-		  result.push({
-			  name: prop.name,
-			  value: (prop.defaultValue && prop.defaultValue.value) || getPropTypeValue(prop.type),
-		  });
-		  return result;
-	  }, [{
-		  name: 'uxpId',
-		  value: `${kebabCase(this.componentName)}-1`,
-	  }]);
+    this.componentProperties = this.componentMetadata.properties.reduce((result:Property[], prop) => {
+      result.push({
+        name: prop.name,
+        value: (prop.defaultValue && prop.defaultValue.value) || getPropTypeValue(prop.type),
+      });
+      return result;
+    }, [{
+      name: 'uxpId',
+      value: `${kebabCase(this.componentName)}-1`,
+    }]);
   }
 
   private generateComponentEnd():string {
-  	if (this.hasChildren()) {
-  		return `${INDENTATION_CHAR}>Put ${this.componentName} contents here</${this.componentName}>`;
-	  }
+    if (this.hasChildren()) {
+      return `${INDENTATION_CHAR}>Put ${this.componentName} contents here</${this.componentName}>`;
+    }
 
-  	return `${INDENTATION_CHAR}/>`;
+    return `${INDENTATION_CHAR}/>`;
   }
 
   private hasChildren():boolean {
-  	return !!this.componentProperties.find(isChildrenProp);
+    return !!this.componentProperties.find(isChildrenProp);
   }
 
   private generateImports():string {
-  	const imports:string[] = [
-	    `import React from 'react';`,
-	    `import ${this.componentName} from '../${this.componentName}';`,
-	  ];
+    const imports:string[] = [
+      `import React from 'react';`,
+      `import ${this.componentName} from '../${this.componentName}';`,
+    ];
 
-  	return imports.join('\n');
+    return imports.join('\n');
   }
 
   private generatePropertiesList():string {
-  	return this.componentProperties.reduce((result:string[], property:Property) => {
-  		if (!isChildrenProp(property)) {
-			  result.push(generatePropertyValuePair(property.name, property.value));
-		  }
+    return this.componentProperties.reduce((result:string[], property:Property) => {
+      if (!isChildrenProp(property)) {
+        result.push(generatePropertyValuePair(property.name, property.value));
+      }
 
-  		return result;
-	  }, []).join('\n');
+      return result;
+    }, []).join('\n');
   }
 }
