e1de955c1c719c26ed43123cdebc57cb484d1f5c
Robert Hilscher <r.hilscher@gmail.com>
2021-02-15 11:19:42 +0100

    code refactor, remove presets

--- packages/uxpin-merge-cli/src/utils/fs/PresetFileGenerator/helpers/getPropTypeValue.ts
+++ packages/uxpin-merge-cli/src/utils/fs/PresetFileGenerator/helpers/getPropTypeValue.ts
@@ -2,23 +2,31 @@ import { reduce } from 'lodash';
 import { PropertyType } from '../../../../steps/serialization/component/implementation/ComponentPropertyDefinition';
 import { getDefaultValueForPropType } from './getDefaultValueForPropType';
 
+enum TYPES {
+  dictionary = 'dictionary',
+  literal = 'literal',
+  shape = 'shape',
+  typedArray = 'typedArray',
+  union = 'union',
+}
+
 export function getPropTypeValue(propertyType?:PropertyType):any {
   if (!propertyType) {
     return '';
   }
 
   switch (propertyType.name) {
-    case 'union':
-      return getPropTypeValue((propertyType as PropertyType<'union'>).structure.elements[0]);
-    case 'literal':
-      return (propertyType as PropertyType<'literal'>).structure.value;
-    case 'typedArray':
-      return [getPropTypeValue((propertyType as PropertyType<'typedArray'>).structure.memberType)];
-    case 'dictionary':
-      return getPropTypeValue((propertyType as PropertyType<'dictionary'>).structure.valueType);
-    case 'shape':
+    case TYPES.union:
+      return getPropTypeValue((propertyType as PropertyType<TYPES.union>).structure.elements[0]);
+    case TYPES.literal:
+      return (propertyType as PropertyType<TYPES.literal>).structure.value;
+    case TYPES.typedArray:
+      return [getPropTypeValue((propertyType as PropertyType<TYPES.typedArray>).structure.memberType)];
+    case TYPES.dictionary:
+      return getPropTypeValue((propertyType as PropertyType<TYPES.dictionary>).structure.valueType);
+    case TYPES.shape:
       return reduce(
-        (propertyType as PropertyType<'shape'>).structure,
+        (propertyType as PropertyType<TYPES.shape>).structure,
         (result, value, propName) => (
           Object.assign(result, { [propName]: getPropTypeValue(value) })
         ), {});
--- packages/uxpin-merge-cli/test/resources/designSystems/withoutPresets/src/components/MenuWithData/presets/0-default.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MenuWithData from '../MenuWithData';
-
-export default (
-  <MenuWithData
-    uxpId="menu-with-data-1"
-    data={[]}
-  />
-);
