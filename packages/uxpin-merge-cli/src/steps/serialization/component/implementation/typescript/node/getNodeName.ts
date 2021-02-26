bbaf40367588b7b2e4ba16a60c3854a9eb9b5360
Robert Hilscher <r.hilscher@gmail.com>
2021-02-26 13:05:06 +0100

    cast node text

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/node/getNodeName.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/node/getNodeName.ts
@@ -14,6 +14,6 @@ export function getNodeName(node:Node):ts.__String | undefined {
   }
 
   if (ts.isLiteralExpression(node.name)) {
-    return node.name.text;
+    return node.name.text as ts.__String;
   }
 }
