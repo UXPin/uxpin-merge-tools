99295e7875840fec544d7e218ae27105408133b7
naohiro-t <naohiro@xenon.io>
2021-03-05 09:01:44 +0900

    refactor getExportDefaultInfo.ts

--- packages/uxpin-merge-cli/src/utils/storybook/config/acornAnalyzers/getExportDefaultInfo.ts
+++ packages/uxpin-merge-cli/src/utils/storybook/config/acornAnalyzers/getExportDefaultInfo.ts
@@ -1,4 +1,4 @@
-import { ExportDefaultDeclaration, Property } from 'acorn-loose';
+import { ExportDefaultDeclaration, Identifier, Literal, Property } from 'acorn-loose';
 
 export interface ExportDefaultInfo {
   title:string;
@@ -12,10 +12,10 @@ export function getExportDefaultInfo(node:ExportDefaultDeclaration):ExportDefaul
   switch (node.declaration.type) {
     case 'ObjectExpression':
       node.declaration.properties.forEach((prop:Property) => {
-        if (prop.key.name === 'title' && prop.value.type === 'Literal') {
-          storiesTitle = prop.value.value;
-        } else if (prop.key.name === 'component' && prop.value.type === 'Identifier') {
-          storiesComponent = prop.value.name;
+        if (isTitle(prop.key) && isLiteral(prop.value)) {
+          storiesTitle = (prop.value as Literal).value;
+        } else if (isComponent(prop.key) && isIdentifier(prop.value)) {
+          storiesComponent = (prop.value as Identifier).name;
         }
       });
       break;
@@ -26,4 +26,20 @@ export function getExportDefaultInfo(node:ExportDefaultDeclaration):ExportDefaul
     localComponentName: storiesComponent,
     title: storiesTitle,
   };
+}
+
+function isTitle(key:Identifier):boolean {
+  return key.name === 'title';
+}
+
+function isComponent(key:Identifier):boolean {
+  return key.name === 'component';
+}
+
+function isLiteral(value:Literal|Identifier):boolean {
+  return value.type === 'Literal';
+}
+
+function isIdentifier(value:Literal|Identifier):boolean {
+  return value.type === 'Identifier';
 }
