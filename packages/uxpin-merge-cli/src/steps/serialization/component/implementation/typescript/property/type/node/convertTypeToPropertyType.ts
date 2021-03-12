c8f70fb4333f1180aa16bf1bd11e3e0d80f73b46
Robert Hilscher <r.hilscher@gmail.com>
2021-03-12 11:31:19 +0100

    tslint fixes, remove console log

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/type/node/convertTypeToPropertyType.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/type/node/convertTypeToPropertyType.ts
@@ -14,7 +14,6 @@ import { serializeUnionType } from './serializeUnionType';
 
 // tslint:disable no-bitwise
 export function convertTypeToPropertyType(context:TSSerializationContext, type:ts.Type):PropertyType {
-  console.log(type);
   if (type.flags & ts.TypeFlags.String) {
     return { name: 'string', structure: {} };
   }
--- packages/uxpin-merge-cli/src/steps/serialization/component/wrappers/isBuiltInWrapper.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/wrappers/isBuiltInWrapper.ts
@@ -1,6 +1,5 @@
 import { BuiltInWrappers } from './ComponentWrapper';
 
 export function isBuiltInWrapper(name:string):name is BuiltInWrappers {
-  // @ts-ignore
-  return Object.values(BuiltInWrappers).includes(name);
+  return Object.values(BuiltInWrappers).includes(name as BuiltInWrappers);
 }
