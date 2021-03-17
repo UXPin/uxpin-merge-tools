7edb6e450d6eca4fc207f05dbe3bc631c52b7705
Robert Hilscher <r.hilscher@gmail.com>
2021-03-17 15:39:42 +0100

    handling HOC

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/isNodeExported.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/isNodeExported.ts
@@ -2,9 +2,15 @@ import { find } from 'lodash';
 import * as ts from 'typescript';
 import { getNodeName } from '../node/getNodeName';
 
+function isNodeNamePassedAsArgument(expression:ts.CallExpression, nodeName:string):boolean {
+  return !!find(expression.arguments, (arg) => getNodeName(arg) === nodeName);
+}
+
 export function isNodeExported(node:ts.Node, nodeName:string):boolean {
-  if (ts.isExportAssignment(node) && getNodeName(node) === nodeName) {
-    return true;
+  if (ts.isExportAssignment(node)
+		&& (getNodeName(node) === nodeName || isNodeNamePassedAsArgument(node.expression as ts.CallExpression, nodeName))
+	) {
+  	return true;
   }
 
   if (ts.isExportDeclaration(node) && node.exportClause) {
--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/node/getNodeName.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/node/getNodeName.ts
@@ -6,11 +6,13 @@ export function getNodeName(node:any):ts.__String | undefined {
       return (node.propertyName && node.propertyName.escapedText) || node.name.escapedText;
     case ts.isExportAssignment(node):
       return ((node as ts.ExportAssignment).expression as ts.Identifier).escapedText;
+    case ts.isIdentifier(node):
+      return node.escapedText;
     case !node.name:
       return;
-    case ts.isIdentifier(node!.name):
-      return node!.name.escapedText;
-    case ts.isLiteralExpression(node!.name):
+    case ts.isIdentifier(node.name):
+      return node.name.escapedText;
+    case ts.isLiteralExpression(node.name):
       return node.name.text as ts.__String;
   }
 }
