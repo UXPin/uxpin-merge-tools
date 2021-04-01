0b9a506f202885258c1feef9e44bc22ae401ca48
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-04-01 10:11:00 +0900

    Removed arui-feather from test resources because it's too old

--- packages/uxpin-merge-cli/test/resources/stubs/aruiFeather.ts
+++
@@ -1,6 +0,0 @@
-import { StubbyStub } from 'stubby';
-import { getStubGetLatestCommitRequest } from './common/getStubGetLatestCommitRequest';
-
-export const aruiFeatherSummaryStub:StubbyStub[] = [
-  getStubGetLatestCommitRequest('14546410449fdaa2014d3f32cf404291cca41c50'),
-];
