b5a2d2ddcad6ac804b0edd11ad60a4d9eddc97b8
Robert Hilscher <r.hilscher@gmail.com>
2021-04-07 17:02:32 +0200

    add comments

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/isFunctionalComponentWithReactForwardRef.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/isFunctionalComponentWithReactForwardRef.ts
@@ -5,16 +5,20 @@ function extractExpression(node:ts.Node):ts.Identifier|ts.PropertyAccessExpressi
     return node.expression as ts.Identifier;
   }
 
+  // export default React.forwardRef(() => {});
   if (ts.isExportAssignment(node)) {
     return (ts.isCallExpression(node.expression) ? node.expression.expression : node.expression) as ts.Identifier;
   }
 
+  // const Component:any = React.forwardRef<HTMLElement, Props>(() => {})
   if (ts.isVariableStatement(node)
-		&& ts.isCallExpression(node.declarationList?.declarations[0]?.initializer as ts.Node)
-	) {
+	  && ts.isCallExpression(node.declarationList?.declarations[0]?.initializer as ts.Node)
+  ) {
     return extractExpression(node.declarationList.declarations[0].initializer as ts.CallExpression);
   }
 
+  // node is variable declaration from declarationList from variable statement
+  // used in getPropsTypeOfFunctionalComponent for extract props
   if (ts.isVariableDeclaration(node) && ts.isCallExpression(node.initializer as ts.CallExpression)) {
     return extractExpression(node.initializer as ts.CallExpression);
   }
@@ -27,11 +31,19 @@ export function isFunctionalComponentWithReactForwardRef(node:ts.Node):boolean {
   	return false;
   }
 
+  /**
+   *  import * as React from 'react'
+   *  React.forwardRef(() => {})
+   */
   if (ts.isPropertyAccessExpression(expression as ts.PropertyAccessExpression)) {
   	return ((expression as ts.PropertyAccessExpression)?.expression as ts.Identifier)?.escapedText === 'React'
 		  && (expression as ts.PropertyAccessExpression)?.name?.escapedText === 'forwardRef';
   }
 
+  /**
+   *  import { forwardRef } from 'react'
+   *  forwardRef(() => {})
+   */
   if (ts.isIdentifier(expression as ts.Identifier)) {
   	return (expression as ts.Identifier)?.escapedText === 'forwardRef';
   }
--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/getPropsTypeOfFunctionalComponent.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/getPropsTypeOfFunctionalComponent.ts
@@ -10,10 +10,12 @@ export function getPropsTypeOfFunctionalComponent(
     return;
   }
 
+  // (props:Props) => {}
   if (func.parameters[0]?.type) {
     return func.parameters[0]?.type;
   }
 
+  // React.forwardRef<HTMLElementProps, Props>((props) => {})
   if (isFunctionalComponentWithReactForwardRef(func.parent)) {
     const typeArguments:ts.NodeArray<ts.TypeNode> | undefined = (func.parent as ts.CallExpression).typeArguments;
 
@@ -24,10 +26,12 @@ export function getPropsTypeOfFunctionalComponent(
 
   const variableType:ts.TypeReferenceNode | undefined = variableDeclaration?.type as ts.TypeReferenceNode;
 
+  // const Component:React.FC<Props> = () =>{};
   if (variableType && variableType.typeArguments && isReactFunctionComponent(variableType.typeName)) {
     return variableType.typeArguments[0];
   }
 
+  // const Component = React.forwardRef<HTMLElementProps, Props>(() => {});
   if (variableDeclaration && isFunctionalComponentWithReactForwardRef(variableDeclaration)) {
     const typeArguments:ts.NodeArray<ts.TypeNode> | undefined = (
       variableDeclaration.initializer as ts.CallExpression
