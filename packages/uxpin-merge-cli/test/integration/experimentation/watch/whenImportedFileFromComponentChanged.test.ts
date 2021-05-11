fbbea57b2b213e140fe9e6571d92bf318a47da67
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-05-12 08:20:57 +0900

    Revert "Increase the timeout for experimentation/watch tests"
    This reverts commit c1f254cc66816b8df90c3dd5885b8ae478350e83.

--- packages/uxpin-merge-cli/test/integration/experimentation/watch/whenImportedFileFromComponentChanged.test.ts
+++ packages/uxpin-merge-cli/test/integration/experimentation/watch/whenImportedFileFromComponentChanged.test.ts
@@ -5,7 +5,7 @@ import { setTimeoutBeforeAll } from '../../../utils/command/setTimeoutBeforeAll'
 import { setupExperimentationServerTest } from '../../../utils/experimentation/setupExperimentationServerTest';
 import { getFileChecksum } from '../../../utils/file/getFileChecksum';
 
-const CURRENT_TIMEOUT:number = 200000;
+const CURRENT_TIMEOUT:number = 100000;
 setTimeoutBeforeAll(CURRENT_TIMEOUT);
 
 describe('Experimental - watch - imported file from component changed', () => {
