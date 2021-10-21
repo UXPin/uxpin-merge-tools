0271c2a1daa6dad35a2b9c78608a01fba1b7dd11
Evan Trujillo <evantrujillo30@gmail.com>
2021-10-21 11:54:46 +0900

    Update getComponentDocUrl.ts

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentDocUrl.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/component/getComponentDocUrl.ts
@@ -5,9 +5,5 @@ import { ComponentDeclaration } from './getPropsTypeAndDefaultProps';
 export function getComponentDocUrl(component:ComponentDeclaration):string | undefined {
   const componentDocUrl:ts.JSDocTag | undefined = getUXpinDocUrlComment(component);
 
-  if (!componentDocUrl?.comment) {
-    return;
-  }
-
-  return componentDocUrl.comment;
+  return componentDocUrl?.comment;
 }
