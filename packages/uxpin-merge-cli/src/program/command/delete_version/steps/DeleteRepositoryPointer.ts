a16797de474da3d9adff4fb1b3fe7e2f8d7d713f
Evan Trujillo <evantrujillo30@gmail.com>
2022-05-17 17:51:02 +0900

    added print message for tag | working delete branch option | added print message for branch

--- packages/uxpin-merge-cli/src/program/command/delete_version/steps/DeleteRepositoryPointer.ts
+++ packages/uxpin-merge-cli/src/program/command/delete_version/steps/DeleteRepositoryPointer.ts
@@ -3,19 +3,42 @@ import { getApiDomain } from '../../../../common/services/UXPin/getApiDomain';
 import { DeleteRepositoryPointerToBranch } from '../../../../common/services/UXPin/DeleteRepositoryPointerToBranch';
 import { DeleteOptions } from '../../../../steps/deleting/DeleteOptions';
 import { DSMetadata } from '../../../../program/DSMeta';
+import { printError, printLine } from '../../../../utils/console/printLine';
 import { VCSDetails } from '../../../../steps/serialization/DesignSystemSnapshot';
+import { PrintColor } from '../../../../../src/utils/console/PrintOptions';
+import { DEFAULT_BRANCH_NAME } from '../../../../../src/common/constants';
 
 export function DeleteRepositoryPointer(deleteOptions:DeleteOptions) {
     return async (designSystem:DSMetadata) => {
         const apiDomain:string = getApiDomain(deleteOptions.uxpinApiDomain!);
         const authToken:string = deleteOptions.token!;
         const vcsDetails:VCSDetails = designSystem.result.vcs;
-        const branch:string | undefined = vcsDetails && vcsDetails.branchName;
+        const commitHash:string = vcsDetails.commitHash;
+        const branch:string | undefined = vcsDetails && vcsDetails.branchName !== DEFAULT_BRANCH_NAME ? vcsDetails.branchName : undefined;
         const tag:string | undefined = deleteOptions.tag;
 
+        if (!tag && !branch) {
+            printError(
+                `🛑 Please specify --branch or --tag and the name of the version you would like to delete.`
+                );
+        }
+
         if (tag) {
-            await DeleteTagWithPrintMessage({apiDomain, authToken, tag})
-            
+            await DeleteTagWithPrintMessage({
+                apiDomain, 
+                authToken, 
+                commitHash, 
+                tag,
+            });
+        }
+
+        if (branch) {
+            await DeleteRepositoryPointerWithPrintMessage({
+                apiDomain,
+                authToken,
+                branch,
+                commitHash,
+            })
         }
     }
 }
@@ -23,12 +46,26 @@ export function DeleteRepositoryPointer(deleteOptions:DeleteOptions) {
 async function DeleteTagWithPrintMessage(opts:{
     apiDomain:string,
     authToken:string,
+    commitHash:string,
     tag:string,
 }):Promise<void> {
-    try {
-        console.log('delete tag!')
         await DeleteTag(opts);
-    } catch (error) {
-        console.log('oh no tag error!')
-    }
+        printLine(
+            `🏷️  Library tag version [${opts.tag}] at commit hash [${opts.commitHash}] has been deleted.`,
+            {color: PrintColor.YELLOW}
+        );
+}
+
+async function DeleteRepositoryPointerWithPrintMessage(opts:{
+    apiDomain:string,
+    authToken:string,
+    branch:string,
+    commitHash:string,
+}):Promise<void> {
+    
+    await DeleteRepositoryPointerToBranch(opts);
+    printLine(
+        `Library branch version [${opts.branch}] at commit hash [${opts.commitHash}] has been deleted.`,
+        {color: PrintColor.YELLOW}
+    )
 }
