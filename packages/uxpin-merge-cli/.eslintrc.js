b7d6585938f8f09b4c847363f48411db66183b69
michaelrambeau <mikeair@gmail.com>
2022-09-02 15:03:15 +0900

    Add ESLint warning rules to make ESLint pass

--- packages/uxpin-merge-cli/.eslintrc.js
+++ packages/uxpin-merge-cli/.eslintrc.js
@@ -6,6 +6,12 @@ module.exports = {
   ignorePatterns: ['*.js'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-types': 'warn', // TODO fix "Don't use `{}` as a type"
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/prefer-as-const': 'warn',
+    'no-prototype-builtins': 'warn',
+    'no-useless-escape': 'warn',
   },
   env: {
     node: true,
