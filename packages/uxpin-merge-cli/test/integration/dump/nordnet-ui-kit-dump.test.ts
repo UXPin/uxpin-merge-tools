fd586ba33f2e43bd8bf556bcf1f141bfc7e24914
michaelrambeau <mikeair@gmail.com>
2022-10-18 14:46:26 +0900

    Restore NordnetUI dump command test

--- packages/uxpin-merge-cli/test/integration/dump/nordnet-ui-kit-dump.test.ts
+++ packages/uxpin-merge-cli/test/integration/dump/nordnet-ui-kit-dump.test.ts
@@ -12,7 +12,7 @@ describe('The dump command', () => {
   const { getTlsPort } = setupStubbyServer(emptyLatestCommitStub);
 
   describe('run for the nordnet-ui-kit repository', () => {
-    xit('prints the JSON describing the full repository', () => {
+    it('prints the JSON describing the full repository', () => {
       // when
       return runUXPinMergeCommand({
         cwd: 'resources/repos/nordnet-ui-kit',
