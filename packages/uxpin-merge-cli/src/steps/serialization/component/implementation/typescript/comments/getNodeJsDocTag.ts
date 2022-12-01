172f052ea27f0e24260b1b67cdd8177cc7fd5f27
michaelrambeau <mikeair@gmail.com>
2022-12-01 11:02:48 +0900

    Fix format

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/comments/getNodeJsDocTag.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/comments/getNodeJsDocTag.ts
@@ -3,7 +3,7 @@ import { CommentTags } from '../../../CommentTags';
 import { getNodeJsDocTags } from './getNodeJsDocTags';
 
 export function getNodeJsDocTag(node: ts.Node, tag: CommentTags): ts.JSDocTag | undefined {
-  const nodeWithJsDoc = getNodeWithJsDoc(node)
+  const nodeWithJsDoc = getNodeWithJsDoc(node);
   const comments: ts.JSDocTag[] = getNodeJsDocTags(nodeWithJsDoc);
 
   return comments.find((comment) => comment.getText().trim().includes(tag));
