7b6e810439c444afe2652607964ee22370a41994
michaelrambeau <mikeair@gmail.com>
2023-01-06 18:14:13 +0900

    Fix some failing tests because of JSDoc annotations

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/symbol/getPropertyCustomDescriptiors.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/typescript/property/symbol/getPropertyCustomDescriptiors.ts
@@ -1,13 +1,14 @@
 import { isValidDescriptor } from '../../../../props/parsing/descriptors/isValidDescriptor';
 import { ParsedPropertyDescriptors } from '../../../ComponentPropertyDefinition';
 import { parseTags } from '../../../javascript/props/parseTags';
+import { getJSDocTagInfoText } from '../../comments/jsdoc-helpers';
 import { ReactPropertySymbol } from './ReactPropertySymbol';
 
 export function getPropertyCustomDescriptors(propertySymbol: ReactPropertySymbol): ParsedPropertyDescriptors {
   const uxpinJsDocTags: string[] = propertySymbol
     .getJsDocTags()
     .filter((jsDocTag) => isValidDescriptor(`@${jsDocTag.name}`))
-    .map((jsDocTag) => `@${jsDocTag.name} ${jsDocTag.text}`);
+    .map((jsDocTag) => `@${jsDocTag.name} ${getJSDocTagInfoText(jsDocTag)}`);
 
   return { descriptors: parseTags(uxpinJsDocTags) };
 }
