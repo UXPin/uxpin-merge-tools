739e1778383ef86b36147c6b84b47864c6343eb2
michaelrambeau <mikeair@gmail.com>
2023-05-09 12:36:42 +0900

    Revert Babel config, don't apply coverage on `test/resources` JS files to fix error on CI

--- packages/uxpin-merge-cli/babel.config.js
+++
@@ -1,9 +0,0 @@
-// Needed to fix error when running coverage on untested files, on CI.
-// > Failed to collect coverage src/resources/Button/Button.js
-// > Support for the experimental syntax 'jsx' isn't currently enabled
-module.exports = {
-  presets: [
-    '@babel/preset-env',
-    ['@babel/preset-react', {runtime: 'automatic'}],
-  ],
-};
