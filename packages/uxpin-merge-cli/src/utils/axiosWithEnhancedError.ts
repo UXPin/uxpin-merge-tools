e3a9c02a73e1943c73b446c527e131156d4f5be4
michaelrambeau <mikeair@gmail.com>
2023-06-29 16:14:50 +0900

    Fix lint error

--- packages/uxpin-merge-cli/src/utils/axiosWithEnhancedError.ts
+++ packages/uxpin-merge-cli/src/utils/axiosWithEnhancedError.ts
@@ -7,7 +7,7 @@ export async function axiosWithEnhancedError(options: AxiosRequestConfig): Axios
   log((options.method || 'GET').toUpperCase(), options.url);
   try {
     const result = await axios(options);
-    log(result.status + ' ' + result.statusText)
+    log(result.status + ' ' + result.statusText);
     return result;
   } catch (error) {
     if ((error as AxiosError).response) {
