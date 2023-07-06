6434cb4cbc8d064112b697682d6c478ecfc35952
michaelrambeau <mikeair@gmail.com>
2023-07-06 17:54:34 +0900

    Fix lint error

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/javascript/serializeJSComponent.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/javascript/serializeJSComponent.ts
@@ -23,7 +23,7 @@ import { getDefaultComponentFrom } from './getDefaultComponentFrom';
 import { isDefaultExported } from './isDefaultExported';
 import { parsePropertyItem } from './parsePropertyItem';
 
-const log = debug('uxpin:serialization')
+const log = debug('uxpin:serialization');
 
 export function serializeJSComponent(component: ComponentImplementationInfo): Promise<ImplSerializationResult> {
   return getDefaultComponentFrom(component.path)
