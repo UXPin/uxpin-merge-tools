1ba3d01f7564bae73e053f174663b4faa08073b3
michaelrambeau <mikeair@gmail.com>
2023-08-17 15:45:09 +0900

    Make `description` available in experimental mode

--- packages/uxpin-merge-cli/src/common/types/ComponentRevision.ts
+++ packages/uxpin-merge-cli/src/common/types/ComponentRevision.ts
@@ -7,4 +7,4 @@ export interface ComponentRevision extends ComponentDefinitionPersistedPart {
 
 export type ComponentDefinitionPersistedPart = Pick<ComponentDefinition, ComponentPersistedProps>;
 
-type ComponentPersistedProps = 'name' | 'info' | 'properties' | 'namespace' | 'wrappers' | 'usePortal';
+type ComponentPersistedProps = 'name' | 'description' | 'info' | 'properties' | 'namespace' | 'wrappers' | 'usePortal';
