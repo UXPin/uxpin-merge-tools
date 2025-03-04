6cb51441943a28c2413880ccfb1a67f61985bba1
Robert Hilscher <r.hilscher@gmail.com>
2025-03-04 14:56:00 +0100

    Allow using jsx code as object or array value in presets

--- packages/uxpin-merge-cli/src/steps/serialization/component/presets/replaceElementsWithReferencesInProps.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/presets/replaceElementsWithReferencesInProps.ts
@@ -2,7 +2,7 @@ import { isArray, isObject, reduce } from 'lodash';
 import { ComponentPresetElementProps } from './ComponentPreset';
 import { getPresetElementReference } from './getPresetElementReference';
 import { isJSXSerializedElement } from './isJSXSerializedElement';
-import { PartialProps } from './jsx/JSXSerializationResult';
+import { JSXSerializedElement, PartialProps } from './jsx/JSXSerializationResult';
 
 export function replaceElementsWithReferencesInProps(props: PartialProps): ComponentPresetElementProps {
   return reduce<ComponentPresetElementProps, PartialProps>(
@@ -28,6 +28,7 @@ function withoutUnsupportedElements(prop: ComponentPresetElementProps | any[] | 
     prop,
     (validProp, subProp, key) => {
       if (isJSXSerializedElement(subProp)) {
+        validProp[key] = makeParsableByUXPin(convertToJSX(subProp));
         return validProp;
       }
 
@@ -41,4 +42,43 @@ function withoutUnsupportedElements(prop: ComponentPresetElementProps | any[] | 
     },
     isArray(prop) ? [] : ({} as ComponentPresetElementProps)
   );
+}
+
+function makeParsableByUXPin(jsx: string | null) {
+  if (!jsx) {
+    return null;
+  }
+
+  return `R$${jsx}$R`;
+}
+
+function convertToJSX(component: JSXSerializedElement | string): string | null {
+  if (typeof component === 'string') {
+    return component;
+  }
+
+  if (!component.name) {
+    return null;
+  }
+
+  const processedChildren = component.children
+    ? component.children.map((child) => convertToJSX(child)).filter(Boolean)
+    : [];
+
+  const propsString = Object.entries(component.props || {})
+    .map(([key, value]) => {
+      if (isJSXSerializedElement(value)) {
+        return `${key}={${convertToJSX(value)}}`;
+      }
+      return `${key}={${JSON.stringify(value)}}`;
+    })
+    .join(' ');
+
+  if (processedChildren.length === 0) {
+    return `<${component.name}${propsString ? ' ' + propsString : ''} />`;
+  }
+
+  return `<${component.name}${propsString ? ' ' + propsString : ''}>${
+    processedChildren.length > 0 ? processedChildren.join(' ') : ''
+  }</${component.name}>`;
 }
