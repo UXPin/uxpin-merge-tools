6b4bc9fc13e7a5274f6920b3177dce7b1517fcff
Stanislav Colotinschi <stan@uxpin.com>
2025-06-09 16:18:11 +0300

    Fix uxpin-storybook-deployer critical vulnerabilities
    See UXPin/uxpin-issues#1680

---
+++ packages/uxpin-storybook-deployer/eslint.config.js
@@ -0,0 +1,34 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    rules: {
+        semi: 2,
+    },
+}, globalIgnores(["bin/**/*", "**/**.js"]), globalIgnores(["**/node_modules"])]);
